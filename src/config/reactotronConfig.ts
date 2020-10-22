b7d83b6ef2184b7ce6370256f1497dbc76ecf86b
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-10-22 17:49:56 -0300

    test: criando estrutura de testes

--- src/config/reactotronConfig.ts
+++ src/config/reactotronConfig.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <div className="teste">
+      <h1>Ola mundo</h1>
+    </div>
+  );
+};
+
+export default SignIn;
