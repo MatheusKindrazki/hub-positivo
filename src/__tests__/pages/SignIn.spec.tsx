e3a26a0856ac4b64d3c4ca76937f0a44a40b6c4f
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-10-22 17:49:56 -0300

    test: criando estrutura de testes

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import SignIn from '../../pages/SignIn';
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: jest.fn(),
+  };
+});
+
+describe('Página de Login', () => {
+  it('Deve ser possível se logar na aplicação', () => {
+    const { debug } = render(<SignIn />);
+
+    debug();
+  });
+});
