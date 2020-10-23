a6598e08a39992bcf35947ad5ffab21307490bff
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-10-22 22:14:12 -0300

    fix: testando reestruturacao dos middlewares

--- src/config/reactotronConfig.ts
+++ src/config/reactotronConfig.ts
@@ -1,22 +1,24 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable no-console */
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
 
+interface Tron {
+  createEnhancer: () => unknown;
+  clear: () => void;
+}
 declare global {
   interface Console {
-    tron: any;
+    tron: Tron;
   }
 }
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure()
+  const tron = (Reactotron.configure()
     .use(reactotronSaga({}))
     .use(reactotronRedux())
-    .connect();
+    .connect() as unknown) as Tron;
 
-  tron.clear?.();
+  tron.clear();
 
   console.tron = tron;
 }
