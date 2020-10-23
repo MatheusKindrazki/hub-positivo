13fa611f0358e03dea2dde1f516ccafafe290ecb
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-10-23 14:29:23 -0300

    feat: criando estrutura da logo

--- src/styles/chakra.ts
+++ src/styles/chakra.ts
@@ -2,6 +2,10 @@ import { theme, DefaultTheme } from '@chakra-ui/core';
 
 const customTheme: DefaultTheme = {
   ...theme,
+  fontSizes: {
+    ...theme.fontSizes,
+    xl: '2rem',
+  },
   fonts: {
     body: 'Nunito, system-ui, sans-serif',
     heading: 'Nunito, system-ui, sans-serif',
@@ -13,6 +17,10 @@ const customTheme: DefaultTheme = {
     medium: 600,
     bold: 700,
   },
+  shadows: {
+    ...theme.shadows,
+    md: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+  },
   radii: {
     ...theme.radii,
     sm: '5px',
@@ -20,14 +28,17 @@ const customTheme: DefaultTheme = {
   },
   colors: {
     ...theme.colors,
-    purple: {
-      ...theme.colors.purple,
-      500: '#8257e5',
+    blue: {
+      ...theme.colors.blue,
+      500: '#1E88E5',
     },
     gray: {
       ...theme.colors.gray,
-      300: '#e1e1e6',
-      600: '#29292e',
+      200: '#ECEFF1',
+      300: '#F4F6F8',
+      400: '#E5E5E5',
+      500: '#7A7A7A',
+      600: '#3C3C3C',
       700: '#202024',
       800: '#121214',
     },
