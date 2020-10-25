08656a80d7331d74502333550d3dc392359858b8
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-10-25 16:13:57 -0300

    refactor: refatorando rotas da aplicacao

--- src/store/modules/auth/sagas.ts
+++ src/store/modules/auth/sagas.ts
@@ -2,8 +2,6 @@ import { all, put, takeLatest } from 'redux-saga/effects';
 
 import { push } from 'connected-react-router';
 
-// import history from '~/services/history';
-
 import { Actions } from './actions';
 
 export function* signIn(): Generator {
