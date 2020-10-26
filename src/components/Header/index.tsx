63bafb71f74d75638a987cb736c4ddccc217e0eb
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-10-26 17:25:03 -0300

    feat: menu mobile finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,42 +1,78 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 
-import { Box, Image, Heading } from '@chakra-ui/core';
+import { Box, Image, Heading, Button } from '@chakra-ui/core';
+import { FiMenu } from 'react-icons/fi';
 import { useMediaQuery } from 'react-responsive';
+import { useTheme } from 'styled-components';
 
 import logo from '~/assets/logo.png';
 
 import DesktopMenu from './DesktopMenu';
+import MobileMenu from './MobileMenu';
+import { RefMenuProps } from './MobileMenu';
 
-const Header: React.FC = () => {
-  const isDesktop = useMediaQuery({ query: '(min-width: 480px)' });
+interface MenuProps {
+  onClick: () => void;
+}
+
+const MenuMobile: React.FC<MenuProps> = ({ onClick }) => {
+  const { colors } = useTheme();
 
   return (
-    <Box
-      p="4"
-      width="100%"
-      height="72px"
-      background="white"
-      display="flex"
-      alignItems="center"
-      justifyContent="space-between"
-      borderBottom="1px solid #D9D9D9"
+    <Button
+      onClick={onClick}
+      backgroundColor="transparent!important"
+      ml="-0.625rem"
+      width="auto"
     >
+      <FiMenu color={colors.blue[500]} size={24} />
+    </Button>
+  );
+};
+
+const Header: React.FC = () => {
+  const menuRef = useRef<RefMenuProps>(null);
+
+  const isDesktop = useMediaQuery({ query: '(min-width: 480px)' });
+
+  const handleClick = useCallback(() => {
+    menuRef.current?.openMenu();
+  }, []);
+
+  return (
+    <>
       <Box
-        className="hub-logo"
+        p="4"
+        width="100%"
+        height="72px"
+        background="white"
         display="flex"
         alignItems="center"
         justifyContent="space-between"
+        borderBottom="1px solid #D9D9D9"
+        position={!isDesktop ? 'fixed' : 'relative'}
+        zIndex={!isDesktop ? 1 : 'auto'}
       >
-        <Image src={logo} maxW="40px" alt="hub digital" />
-        <Box alignItems="center" justifyContent="center" ml="2">
-          <Heading as="h1" color="blue.500" size="md">
-            Hub Positivo
-          </Heading>
+        <Box
+          className="hub-logo"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          {!isDesktop && <MenuMobile onClick={handleClick} />}
+          <Image src={logo} maxW="40px" alt="hub digital" />
+          <Box alignItems="center" justifyContent="center" ml="2">
+            <Heading as="h1" color="blue.500" size="md">
+              Hub Positivo
+            </Heading>
+          </Box>
         </Box>
-      </Box>
 
-      {isDesktop && <DesktopMenu />}
-    </Box>
+        {isDesktop && <DesktopMenu />}
+        {!isDesktop && <MobileMenu ref={menuRef} />}
+      </Box>
+      {!isDesktop && <Box height="72px" />}
+    </>
   );
 };
 
