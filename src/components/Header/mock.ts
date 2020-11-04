afd5f35cf5985e86537ff689d9895eb736e3eec2
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-11-04 17:29:11 -0300

    feat: simulacao de troca de perfil com usuario autenticado

---
+++ src/components/Header/mock.ts
@@ -0,0 +1,28 @@
+const accessData = [
+  {
+    value: '1',
+    label: 'Gestor',
+    icon: 'gestor',
+    colorProfile: 'gestor',
+  },
+  {
+    value: '2',
+    label: 'Professor',
+    icon: 'professor',
+    colorProfile: 'professor',
+  },
+  {
+    value: '3',
+    label: 'Aluno',
+    icon: 'aluno',
+    colorProfile: 'aluno',
+  },
+  {
+    value: '4',
+    label: 'Família',
+    icon: 'familia',
+    colorProfile: 'familia',
+  },
+];
+
+export default accessData;
