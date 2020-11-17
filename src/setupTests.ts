9caf643674b83f6a55b6fdff3818c14a9bc1a73a
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-11-17 14:11:00 -0300

    feat: ajustando package.json

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,3 +4,29 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('react-toastify');
+
+jest.mock('react-redux', () => {
+  return {
+    useDispatch: jest.fn().mockReturnValue(jest.fn()),
+    useSelector: jest.fn().mockReturnValue(() => {
+      return {
+        profile: 'professor',
+      };
+    }),
+  };
+});
+
+jest.mock('polished', () => {
+  return {
+    lighten: jest.fn(),
+    darken: jest.fn(),
+  };
+});
+
+jest.mock('react-router-dom', () => {
+  return {
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
