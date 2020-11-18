b0967bdfc2a55cb0f3225cc170a5d024aebad8d5
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-11-18 15:13:20 -0300

    fix: alterando GTM

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import TagManager, { TagManagerArgs } from 'react-gtm-module';
+
 import AppProvider from './hooks';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
+
+const tagManagerArgs: TagManagerArgs = {
+  gtmId: 'GTM-PCPNTVS',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const App: React.FC = () => {
   return (
