4297a80d90d218672c42401a679ff3fa9c7b0bd8
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-11-24 19:59:31 -0300

    chore: migrando configuracao do commitlint

--- packages/apps/hub-web/src/App.tsx
+++ packages/apps/hub-web/src/App.tsx
@@ -5,11 +5,7 @@ import ThemeProvider from '@hub/common/layout/Provider'
 import Routes from '~/routes'
 
 const App: React.FC = () => {
-  return (
-    <ThemeProvider>
-      <Routes />
-    </ThemeProvider>
-  )
+  return <ThemeProvider></ThemeProvider>
 }
 
 export default App
