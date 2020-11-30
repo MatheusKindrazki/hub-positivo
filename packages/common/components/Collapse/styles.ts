57409e2515949413929159457d0195c19c7c2d76
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-11-30 09:26:45 -0300

    feature: Adicionando tooltip

--- packages/common/components/Collapse/styles.ts
+++ packages/common/components/Collapse/styles.ts
@@ -1,11 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-  .collapse-home {
-    /* + .collapse-home {
-      margin-top: 30px;
-      padding-top: 20px;
-      border-top: 1px solid #C4C4C4;
-    } */
+
+  .ReactCollapse--collapse {
+    transition: height 300ms ease-in-out;
   }
+
 `
