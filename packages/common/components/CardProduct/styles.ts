dc8a0dd894d55e54ced07e7b1daad3c7a2c2a0b5
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-12-05 22:55:25 -0300

    fix: desativando cards sem link

--- packages/common/components/CardProduct/styles.ts
+++ packages/common/components/CardProduct/styles.ts
@@ -6,5 +6,9 @@ export const Container = styled(Box)`
   &.disabled {
     opacity: 0.9;
     pointer-events: none;
+    position: relative;
+    filter: grayscale(0.9);
+    box-shadow: none;
+    opacity: 0.8;
   }
 `
