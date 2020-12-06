aa808b80809b042106dd40b054a300e39c0be65e
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-12-05 23:01:46 -0300

    fix: adicionando animacao box-shadow cardProduct

--- packages/common/layout/styles/chackra.ts
+++ packages/common/layout/styles/chackra.ts
@@ -21,8 +21,9 @@ const customTheme: DefaultTheme = {
   },
   shadows: {
     ...theme.shadows,
-    sm: '0px 3px 6px rgba(0, 0, 0, 0.3)',
-    md: '0px 4px 4px rgba(0, 0, 0, 0.25)'
+    sm: '0px 1px 2px rgba(0, 0, 0, 0.15)',
+    md: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    'dark-lg': '0px 3px 4px rgba(0, 0, 0, 0.3)'
   },
   radii: {
     ...theme.radii,
