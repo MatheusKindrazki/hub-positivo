0804379fc9d4dee9d25ba302dd45339e0f90e97f
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-12-11 18:06:11 -0300

    fix: salvando tour na api

--- packages/apps/hub-web/src/store/modules/rootSaga.ts
+++ packages/apps/hub-web/src/store/modules/rootSaga.ts
@@ -4,7 +4,8 @@ import auth from './auth/sagas'
 import authProduct from './authProduct/sagas'
 import levelEducation from './levelEducation/sagas'
 import products from './products/sagas'
+import tour from './tour/sagas'
 
 export default function* rootSaga(): Generator {
-  return yield all([auth, products, authProduct, levelEducation])
+  return yield all([auth, products, authProduct, levelEducation, tour])
 }
