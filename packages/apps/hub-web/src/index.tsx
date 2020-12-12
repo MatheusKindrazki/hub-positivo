20243e9a8782b3dc3e3efe10c721b9bf8fbbea1c
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-12-12 16:20:43 -0300

    fix: adicionando GTM do HUB

--- packages/apps/hub-web/src/index.tsx
+++ packages/apps/hub-web/src/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import ReactDOM from 'react-dom'
-
 import 'react-toastify/dist/ReactToastify.css'
 
 import App from './App'
+
+Sentry.init({
+  dsn:
+    'https://47feebc700d446a7a58574fbe0acbec2@o490568.ingest.sentry.io/5554689',
+  autoSessionTracking: true,
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0
+})
 
 ReactDOM.render(<App />, document.getElementById('hub-psd'))
