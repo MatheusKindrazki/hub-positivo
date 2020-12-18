0025ea7fa6ce40effd61b07c030181ef56724589
Felipe Vieira <flpvieira86@gmail.com>
2020-12-18 08:36:00 -0300

    refactor: refatorando para utilizar o componente GoBack

--- packages/apps/hub-web/src/pages/Auth/ExpiredToken/index.tsx
+++ packages/apps/hub-web/src/pages/Auth/ExpiredToken/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-import { Box, Button, Heading, Text } from '@hub/common/components'
+import { Box, Button, Text } from '@hub/common/components'
 import { Button as ButtonForm } from '@hub/common/components/Form'
-import { ArrowLeft } from '@hub/common/components/Icons'
 
 import { toast } from 'react-toastify'
+
+import GoBack from '~/components/GoBack'
 
 import history from '~/services/history'
 
@@ -17,14 +18,9 @@ const ExpiredToken: React.FC = () => {
 
   return (
     <Box p="6">
-      <Box d="flex" alignItems="center" justifyContent="flex-start" mb="2">
-        <Button colorScheme="blue" variant="link" justifyContent="flex-start">
-          <Box as={ArrowLeft} color="blue.500" size={24} />
-        </Button>
-        <Heading color="black" fontSize="xl">
-          O link expirou
-        </Heading>
-      </Box>
+      <GoBack colorScheme="blue" onClick={handleLoginRedirect}>
+        O link expirou
+      </GoBack>
       <Text fontSize="md" color="gray.500" mb="6">
         Solicite um novo link para redefinir sua senha.
       </Text>
