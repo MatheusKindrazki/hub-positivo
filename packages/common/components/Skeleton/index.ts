4b51f47945040302eca954bb3d0798849ca59445
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-12-28 08:40:33 -0300

    feat: adicionando Skeleton Hub nos components em comum

---
+++ packages/common/components/Skeleton/index.ts
@@ -0,0 +1,17 @@
+import {
+  Skeleton,
+  SkeletonProps,
+  SkeletonTextProps,
+  SkeletonCircle,
+  SkeletonText,
+  Stack
+} from '@chakra-ui/react'
+
+export default {
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+  Stack
+}
+
+export type { SkeletonProps, SkeletonTextProps }
