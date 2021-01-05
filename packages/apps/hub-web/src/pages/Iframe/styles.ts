90361e90a46a5aeefee1e7296e1225f84314b22a
gabryelryba <gabryel.ryba@gmail.com>
2021-01-05 13:42:24 -0300

    fix: margem do header reduzida com media query
    reduzida a margem do header para se adequar ao tamanho do header mobile

--- packages/apps/hub-web/src/pages/Iframe/styles.ts
+++ packages/apps/hub-web/src/pages/Iframe/styles.ts
@@ -2,9 +2,14 @@ import styled from 'styled-components'
 
 export const IframeContainer = styled.div`
   position: relative;
-  margin-top: 72px;
+  margin-top: 41px;
   width: 100%;
-  height: calc(100vh - 72px);
+  height: calc(100vh - 41px);
+
+  @media (min-width: 480px) {
+    margin-top: 72px;
+    height: calc(100vh - 72px);
+  }
 
   iframe {
     position: relative;
