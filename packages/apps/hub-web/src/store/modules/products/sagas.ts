d8db47168ca61e5754698b77b7f368172ee50420
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-01-06 18:52:15 -0300

    refactor: removendo api arvore e mhund

--- packages/apps/hub-web/src/store/modules/products/sagas.ts
+++ packages/apps/hub-web/src/store/modules/products/sagas.ts
@@ -1,6 +1,5 @@
 import { all, call, put, takeLatest } from 'redux-saga/effects'
 
-import api, { apiLivro, apiMHUND, apiAuthProduct } from '@hub/api'
 import { toast } from '@hub/common/utils'
 
 import { ApiResponse } from 'apisauce'
@@ -19,9 +18,6 @@ export function* getProducts(): Generator {
 
   const { guid } = store.getState().profile
   const { level } = store.getState().levelEducation
-
-  const auth = store.getState().auth
-  const profile = store.getState().profile
 
   const { user, school } = store.getState().user
   let query: string
@@ -44,74 +40,13 @@ export function* getProducts(): Generator {
     return
   }
 
-  /* Inicio ( Cod: Temporário ) */
-  const authTheProduct = {
-    product: 'mhund',
-    token: auth.token,
-    logged_in: {
-      school: {
-        name: school?.label,
-        id: school?.value,
-        class: null
-      },
-      profile: profile.guid,
-      user_id: null
-    },
-    expire_in: auth.exp
-  }
-
-  const responseGUID = yield call(() => {
-    return apiAuthProduct.post('api/TokenStorage', authTheProduct)
-  })
-
-  const { data: dataGUID } = responseGUID as ApiResponse<object>
-
-  const apiLivrosResponse = yield call(() => {
-    return apiLivro.get(`checkuser?integrationId=${user?.integration_id || 0}`)
-  })
-
-  const { data: dataLivros } = apiLivrosResponse as ApiResponse<{
-    redirects_to: string
-  }>
-
-  // https://appsv-saedigital-dev-001.azurewebsites.net/api/mhund/CheckUser?guid=60111456-7f53-4652-9baa-89fcdafecebb
-  const apiMHUNDResponse = yield call(() => {
-    return apiMHUND.get(`checkuser?guid=${dataGUID || 0}`)
-  })
-  const { data: dataMHUND } = apiMHUNDResponse as ApiResponse<{
-    redirects_to: string
-  }>
-
-  const alterData = data?.map(d => {
-    return {
-      ...d,
-      solucoes: d?.solucoes?.map(s => {
-        if (s.nome === 'Árvore Livros') {
-          return {
-            ...s,
-            link: dataLivros?.redirects_to || '',
-            ativo: !!dataLivros?.redirects_to
-          }
-        } else if (s.nome === 'Gestão Escolar - Mhund') {
-          return {
-            ...s,
-            link: dataMHUND?.redirects_to || '',
-            ativo: !!dataMHUND?.redirects_to
-          }
-        } else return s
-      })
-    }
-  })
-
-  /* Fim ( Cod: Temporário ) */
-
   yield put(loading(false))
 
   yield put(getTourRequest())
 
   yield put(
     productSuccess({
-      data: alterData
+      data: data
         ?.filter(e => e.ativo)
         .map(c => {
           return {
