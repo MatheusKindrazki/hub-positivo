02273cb46d5c08e8baaa7cdb9675a9bfa98f215d
Matheus Kindrazki <matheussaldanha@positivosolucoes.com.br>
2021-01-08 16:12:41 -0300

    refactor: separando sentry e global info em arquivos

---
+++ packages/apps/hub-web/src/hooks/useSendGlobalInfo.ts
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+
+import { useSelector } from 'react-redux'
+
+const useSendGlobalInfo = (): void => {
+  const { user, school } = useSelector((state: Store.State) => state.user)
+  const { name } = useSelector((state: Store.State) => state.profile)
+  const { level } = useSelector((state: Store.State) => state.levelEducation)
+
+  useEffect(() => {
+    window.__HUB_USER_INFO__ = {
+      id: user?.guid as string,
+      educational_stage: level,
+      name: user?.name as string,
+      role: name,
+      school: school?.label as string
+    }
+  }, [level, name, school?.label, user])
+}
+export { useSendGlobalInfo }
