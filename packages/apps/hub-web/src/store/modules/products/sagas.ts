00881cd633223eb7ad8c05a9359324a0a88969cc
Matheus Kindrazki <matheussaldanha@positivosolucoes.com.br>
2021-01-13 18:12:51 -0300

    refactor: Refatorando produtos

--- packages/apps/hub-web/src/store/modules/products/sagas.ts
+++ packages/apps/hub-web/src/store/modules/products/sagas.ts
@@ -1,6 +1,6 @@
 import { all, call, put, takeLatest } from 'redux-saga/effects'
 
-import api, { apiAuthProduct, apiLivro, apiMHUND } from '@hub/api'
+import api from '@hub/api'
 import { toast } from '@hub/common/utils'
 
 import { ApiResponse } from 'apisauce'
@@ -10,7 +10,7 @@ import { getTourRequest } from '~/store/modules/tour/actions'
 
 import { loading } from '../global/actions'
 import { Actions, productSuccess } from './actions'
-import { CardProduct } from './types'
+import { CardProduct, CardProps } from './types'
 
 export function* getProducts(): Generator {
   yield put(loading(true))
@@ -19,9 +19,6 @@ export function* getProducts(): Generator {
 
   const { guid } = store.getState().profile
   const { level } = store.getState().levelEducation
-
-  const auth = store.getState().auth
-  const profile = store.getState().profile
 
   const { user, school } = store.getState().user
   let query: string
@@ -45,65 +42,19 @@ export function* getProducts(): Generator {
     return
   }
 
-  /* Inicio ( Cod: Temporário ) */
-  const authTheProduct = {
-    product: 'mhund',
-    token: auth.token,
-    logged_in: {
-      school: {
-        name: school?.label,
-        id: school?.value,
-        class: null
-      },
-      profile: profile.guid,
-      user_id: null
-    },
-    expire_in: auth.exp
+  const cardName = {
+    'Árvore Livros': 'apisae',
+    'Gestão Escolar - Mhund': 'mhund'
   }
 
-  const responseGUID = yield call(() => {
-    return apiAuthProduct.post('api/TokenStorage', authTheProduct)
-  })
-
-  const { data: dataGUID } = responseGUID as ApiResponse<object>
-
-  const apiLivrosResponse = yield call(() => {
-    return apiLivro.get(`checkuser?integrationId=${user?.integration_id || 0}`)
-  })
-
-  const { data: dataLivros } = apiLivrosResponse as ApiResponse<{
-    redirects_to: string
-  }>
-
-  const apiMHUNDResponse = yield call(() => {
-    return apiMHUND.get(`checkuser?guid=${dataGUID || 0}`)
-  })
-  const { data: dataMHUND } = apiMHUNDResponse as ApiResponse<{
-    redirects_to: string
-  }>
-
-  const alterData = data?.map(d => {
-    return {
-      ...d,
-      solucoes: d?.solucoes?.map(s => {
-        if (s.nome === 'Árvore Livros') {
-          return {
-            ...s,
-            link: dataLivros?.redirects_to || '',
-            ativo: !!dataLivros?.redirects_to
-          }
-        } else if (s.nome === 'Gestão Escolar - Mhund') {
-          return {
-            ...s,
-            link: dataMHUND?.redirects_to || '',
-            ativo: !!dataMHUND?.redirects_to
-          }
-        } else return s
-      })
-    }
-  })
-
-  /* Fim ( Cod: Temporário ) */
+  const alterData = data?.filter(e =>
+    e.solucoes.filter(s => {
+      if (cardName[s.nome as CardProps]) {
+        return { ...s, type: cardName[s.nome as CardProps] }
+      }
+      return s
+    })
+  )
 
   yield put(loading(false))
 
