91aab5d2c50537ba6b9249c60931618a19db5c4e
gabryelryba <gabryel.ryba@gmail.com>
2021-01-14 11:47:23 -0300

    fix: corrigida largura do menu de ferramentas
    corrigido erro de estilo que fazia com que o menu ficasse cortado em telas com largura superior a
    360px

--- packages/apps/hub-web/src/layouts/Iframe/components/Header/HeaderMobile.tsx
+++ packages/apps/hub-web/src/layouts/Iframe/components/Header/HeaderMobile.tsx
@@ -7,6 +7,7 @@ import {
   SpinnerLoader,
   SimpleGrid
 } from '@hub/common/components'
+import Drawer from '@hub/common/components/Drawer'
 import { DotsNine } from '@hub/common/components/Icons'
 
 import classNames from 'classnames'
@@ -21,6 +22,9 @@ import GlobalStyle from './stylesMobile'
 const HeaderMobile: React.FC<HeaderProps> = ({ cards, handlePush }) => {
   const [search, setSearch] = useState('')
   const [show, setShow] = useState(false)
+
+  const { DrawerContainer, DrawerContent, useDisclosure } = Drawer
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const filterCards = useMemo(
     () => cardFilter({ data: cards || [], search: search }),
@@ -37,77 +41,89 @@ const HeaderMobile: React.FC<HeaderProps> = ({ cards, handlePush }) => {
         mx="1"
         type="button"
         className="button-header"
-        onClick={() => setShow(!show)}
+        onClick={() => {
+          if (show) {
+            onClose()
+          } else {
+            onOpen()
+          }
+          setShow(!show)
+        }}
       >
         <Box as={DotsNine} size={24} />
       </Button>
-      <CSSTransition
-        in={show}
-        timeout={400}
-        classNames="hub-menu"
-        unmountOnExit
+      <DrawerContainer
+        isOpen={isOpen}
+        placement="right"
+        onClose={() => console.log('close')}
       >
-        <Box
-          borderRadius="4px"
-          boxShadow="dark-lg"
-          border="1px solid #DADADA"
-          w="22.5rem"
-          h="auto"
-          overflow="auto"
-          background="white!important"
-          className={classNames({
-            'hub-items': true
-          })}
-          paddingTop="2.5rem"
+        <DrawerContent
+          mt={['41px', '41px']}
+          position="relative"
+          maxWidth="100vw"
         >
-          <Search handleChange={setSearch} style={{ zIndex: 9 }} />
           <Box
-            w="100%"
+            borderRadius="4px"
+            boxShadow="dark-lg"
+            border="1px solid #DADADA"
+            w="100vw"
             h="auto"
-            maxHeight="90vh"
             overflow="auto"
-            paddingTop="3.5rem"
-            px="6"
-            py="4"
+            background="white!important"
+            className={classNames({
+              'hub-items': true
+            })}
+            paddingTop="2.5rem"
           >
-            {!cards || !cards.length ? (
-              <Box
-                w="100%"
-                d="flex"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <SpinnerLoader loading={true} color="var(--hub-base-color)" />
-              </Box>
-            ) : null}
-            {filterCards?.map((card, i) => (
-              <div key={String(i)}>
-                <Box mb="4">
-                  <Heading
-                    as="h4"
-                    className={classNames({ margin: i !== 0 })}
-                    fontSize="sm"
-                  >
-                    {card.nome}
-                  </Heading>
+            <Search handleChange={setSearch} style={{ zIndex: 9 }} />
+            <Box
+              w="100%"
+              h="auto"
+              maxHeight="90vh"
+              overflow="auto"
+              paddingTop="3.5rem"
+              px="6"
+              py="4"
+            >
+              {!cards || !cards.length ? (
+                <Box
+                  w="100%"
+                  d="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <SpinnerLoader loading={true} color="var(--hub-base-color)" />
                 </Box>
-                <SimpleGrid templateColumns="repeat(3, 1fr)" spacing={3}>
-                  {card.solucoes?.map(solucao => (
-                    <Card
-                      key={Math.random()}
-                      card={{ ...solucao, cor: card.cor }}
-                      onClick={e => {
-                        handlePush(e)
-                        setShow(false)
-                      }}
-                    />
-                  ))}
-                </SimpleGrid>
-              </div>
-            ))}
+              ) : null}
+              {filterCards?.map((card, i) => (
+                <div key={String(i)}>
+                  <Box mb="4">
+                    <Heading
+                      as="h4"
+                      className={classNames({ margin: i !== 0 })}
+                      fontSize="sm"
+                    >
+                      {card.nome}
+                    </Heading>
+                  </Box>
+                  <SimpleGrid templateColumns="repeat(3, 1fr)" spacing={3}>
+                    {card.solucoes?.map(solucao => (
+                      <Card
+                        key={Math.random()}
+                        card={{ ...solucao, cor: card.cor }}
+                        onClick={e => {
+                          handlePush(e)
+                          setShow(false)
+                        }}
+                      />
+                    ))}
+                  </SimpleGrid>
+                </div>
+              ))}
+            </Box>
           </Box>
-        </Box>
-      </CSSTransition>
+        </DrawerContent>
+      </DrawerContainer>
       <GlobalStyle />
     </div>
   )
