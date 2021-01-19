7bad3650f253ce93d3084dddb2a1c9ac6c525ebc
Matheus Kindrazki <matheussaldanha@positivosolucoes.com.br>
2021-01-19 20:18:55 -0300

    refactor: ajuste no carregamento dinamico, verificação em JSON post message

--- packages/apps/hub-web/src/hooks/usePostMessage.ts
+++ packages/apps/hub-web/src/hooks/usePostMessage.ts
@@ -2,6 +2,8 @@ import { useEffect } from 'react'
 
 import { useHistory, useLocation, useParams } from 'react-router-dom'
 
+import hasJsonStructure from '~/utils/hasJsonStructure'
+
 interface MessageProps {
   event: 'history-change' | undefined
   data: string
@@ -21,6 +23,8 @@ const usePostMessage = (): void => {
   useEffect(() => {
     function handleEvent(event: { data: string }): void {
       if (typeof event.data === 'string') {
+        if (!hasJsonStructure(event.data)) return
+
         const parseData = (JSON.parse(event.data) as MessageProps) || undefined
 
         if (parseData.event === 'history-change') {
