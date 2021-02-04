ed88a6d798e71e31154d4eeca9cfc0a0b9be6872
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-02-04 15:41:42 -0300

    test(hasjsonstructure.spec.ts): verificando estruturas json

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,18 @@ module.exports = {
   clearMocks: true,
   collectCoverage: true,
   verbose: true,
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
-  collectCoverageFrom: [
-    '<rootDir>/packages/**/.tsx',
-    '<rootDir>/packages/**/.ts'
-  ],
-  projects: ['<rootDir>/packages/**/jest.config.js'],
-  preset: 'ts-jest',
   testEnvironment: 'node',
+  preset: 'ts-jest',
+  projects: ['<rootDir>/packages/**/jest.config.js'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
   globals: {
     'ts-jest': {
       diagnostics: false
--- packages/common/jest.config.js
+++ packages/common/jest.config.js
@@ -3,5 +3,12 @@ const { name } = require('./package.json');
 module.exports = {
   displayName: name,
   name,
-  preset: 'ts-jest'
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    "~(.*)$": "<rootDir>/src/$1"
+  },
+  collectCoverageFrom: [
+    '<rootDir>/packages/**/.tsx',
+    '<rootDir>/packages/**/.ts'
+  ],
 };
