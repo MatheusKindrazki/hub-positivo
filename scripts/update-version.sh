b9c73443b09a461168927d72a82877a8f167b0aa
Matheus Kindrazki <matheussaldanha@positivosolucoes.com.br>
2021-02-11 18:09:23 +0000

    Added update-version.sh

---
+++ scripts/update-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+echo $PACKAGE_VERSION
+
+npx json --in-place -f 'packages/common/package.json' -e 'this.version="'$PACKAGE_VERSION'"';
+npx json --in-place -f 'packages/apps/hub-web/package.json' -e 'this.version="'$PACKAGE_VERSION'"';
