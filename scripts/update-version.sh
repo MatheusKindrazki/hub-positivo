3de51655dfd40ddd24eda0e7d9465ee648019ca7
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-02-11 15:32:36 -0300

    chore: atualizando pacote

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -5,7 +5,7 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
-echo $PACKAGE_VERSION
+echo 'Versão atual do pacote:' $PACKAGE_VERSION
 
-npx json --in-place -f 'packages/common/package.json' -e 'this.version="'$PACKAGE_VERSION'"';
-npx json --in-place -f 'packages/apps/hub-web/package.json' -e 'this.version="'$PACKAGE_VERSION'"';
+npx json --in-place -f 'packages/common/package.json' -e 'this.version="'PACKAGE_VERSION'"';
+npx json --in-place -f 'packages/apps/hub-web/package.json' -e 'this.version="'PACKAGE_VERSION'"';
