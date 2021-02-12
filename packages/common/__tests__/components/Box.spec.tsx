366b426a44ea4d1f443d8e84d2729d7736a70c0e
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-02-12 11:50:59 -0300

    test(adicionando testes para os componentes comuns do repositorio): adicionar testes
    Testar componentes Box, SpinnerLoader, Text e Tooltip

---
+++ packages/common/__tests__/components/Box.spec.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+
+import { render } from '@hub/test-utils'
+
+import Box from '../../components/Box'
+
+describe('Box renders without crashing', () => {
+  const childrenValue = 'testing children'
+  const wrapper = render(<Box>{childrenValue}</Box>)
+  const { getByText } = wrapper
+
+  it('Box has children', () => {
+    const children = getByText(childrenValue)
+    expect(children).toBeInTheDocument()
+  })
+
+  it('Box matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
