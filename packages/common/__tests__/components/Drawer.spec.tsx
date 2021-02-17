0023910864aa3e17b7e7eaec5b9cd46183e6b756
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-02-17 13:13:22 -0300

    test(inicializar testes do drawer): testes automatizados

---
+++ packages/common/__tests__/components/Drawer.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { render } from '@hub/test-utils'
+
+import {
+  DrawerCloseButton,
+  DrawerBody,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay
+} from '../../components/Drawer'
+
+describe('', () => {
+  it('', () => {
+    // const wrapper = render(<DrawerBody />)
+  })
+})
