8ce80f2b8e4fd22470310eb496feb128874167f1
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-02-17 17:01:00 -0300

    test(testando drawer component): testes automatizados

--- packages/common/__tests__/components/Drawer.spec.tsx
+++ packages/common/__tests__/components/Drawer.spec.tsx
@@ -1,18 +1,84 @@
 import React from 'react'
 
+import '@testing-library/jest-dom'
+
+import { renderHook } from '@testing-library/react-hooks'
+
 import { render } from '@hub/test-utils'
 
+import { Text } from '@chakra-ui/react'
+import * as chacka from '@chakra-ui/react'
+
+import Drawer from '../../components/Drawer'
 import {
   DrawerCloseButton,
   DrawerBody,
   DrawerContent,
   DrawerFooter,
   DrawerHeader,
-  DrawerOverlay
+  DrawerOverlay,
+  useDisclosure
 } from '../../components/Drawer'
 
-describe('', () => {
-  it('', () => {
-    // const wrapper = render(<DrawerBody />)
+const DrawerComponent = () => {
+  const onClose = jest.fn()
+  return (
+    <>
+      <Drawer isOpen={true} placement="right" onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>
+              <Text>Header</Text>
+            </DrawerHeader>
+            <DrawerBody>
+              <Text>Body</Text>
+            </DrawerBody>
+            <DrawerFooter>
+              <Text>Footer</Text>
+            </DrawerFooter>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
+  )
+}
+
+describe('Testing if Drawer component render correctly', () => {
+  it('should render open, as well must render Drawer Header, Body and Footer', () => {
+    const { getByText } = render(<DrawerComponent />)
+
+    const headerText = getByText('Header')
+    const bodyText = getByText('Body')
+    const footerText = getByText('Footer')
+
+    expect(headerText).toBeInTheDocument()
+    expect(bodyText).toBeInTheDocument()
+    expect(footerText).toBeInTheDocument()
+  })
+
+  it('Drawer matches snapshot', () => {
+    const wrapper = render(<DrawerComponent />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe('testing useDesclosure', () => {
+  beforeAll(() => {
+    jest.unmock('@chakra-ui/react')
+  })
+  const {
+    result: { current }
+  } = renderHook(() => useDisclosure())
+
+  it('should returns the correct values', () => {
+    expect(current.isOpen).toBeFalsy()
+    expect(current.isControlled).toBeFalsy()
+
+    expect(typeof current.onClose).toBe('function')
+    expect(typeof current.onOpen).toBe('function')
+    expect(typeof current.onToggle).toBe('function')
+    expect(typeof current.getButtonProps).toBe('function')
+    expect(typeof current.getDisclosureProps).toBe('function')
   })
 })
