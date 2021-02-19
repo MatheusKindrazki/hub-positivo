2a5e7f3c262cc5aad3aeb1a056f8661c8f6ab60d
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-02-19 08:22:40 -0300

    test(refatorar testes do component image): testes automatizados

--- packages/common/__tests__/components/Image.spec.tsx
+++ packages/common/__tests__/components/Image.spec.tsx
@@ -6,14 +6,16 @@ import '@testing-library/jest-dom'
 import Image from '../../components/Image'
 
 describe('Image renders without crashing', () => {
-  it('Image matches snapshot', () => {
-    const altText = 'alt text'
-    const wrapper = render(<Image alt={altText} />)
+  const altText = 'alt text'
+  const wrapper = render(<Image alt={altText} />)
+  it('Should render image with correct alt text', () => {
     const { getByAltText } = wrapper
     const image = getByAltText(altText)
 
     expect(image).toBeInTheDocument()
     expect(image).toHaveAttribute('alt', altText)
+  })
+  it('Image matches snapshot', () => {
     expect(wrapper).toMatchSnapshot()
   })
 })
