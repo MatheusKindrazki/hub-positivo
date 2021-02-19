096d1bd45769291e4ca922ae465da604c4b25b39
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-02-19 10:56:29 -0300

    test(testando skeleton component): testes automatizados

--- packages/common/__tests__/components/Skeleton.spec.tsx
+++ packages/common/__tests__/components/Skeleton.spec.tsx
@@ -1,15 +1,57 @@
 import React from 'react'
 
+import { render } from '@hub/test-utils'
+
 import Skeleton, {
   SkeletonProps,
+  Skeleton as SkeletonNotDefault,
   SkeletonTextProps,
   SkeletonCircle,
   SkeletonText,
   Stack
 } from '../../components/Skeleton'
+import '@testing-library/jest-dom'
 
-describe('Skeleton component should work properly', () => {
-  it('init', () => {
-    expect(1).toBe(1)
+const skeletonProps: SkeletonProps = {
+  height: '20px'
+}
+
+const skeletonTextProps: SkeletonTextProps = {
+  fontSize: '1em'
+}
+
+describe('Skeleton renders without crashing', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      value: jest.fn().mockImplementation(() => ({
+        addListener: jest.fn(),
+        removeListener: jest.fn()
+      }))
+    })
+  })
+
+  it('Stack matches snapshot', () => {
+    const wrapper = render(<Stack />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('Skeleton matches snapshot', () => {
+    const wrapper = render(
+      <>
+        <Skeleton {...skeletonProps} />
+        <SkeletonNotDefault />
+      </>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('SkeletonCircle matches snapshot', () => {
+    const wrapper = render(<SkeletonCircle />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('SkeletonText matches snapshot', () => {
+    const wrapper = render(<SkeletonText {...skeletonTextProps} />)
+    expect(wrapper).toMatchSnapshot()
   })
 })
