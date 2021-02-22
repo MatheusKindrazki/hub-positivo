cea22b67583098121fc5633a7d92d28232e7af0b
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-02-22 11:52:21 -0300

    test(testando estilos do componente select): testes automatizados

--- packages/common/__tests__/components/Select.spec.tsx
+++ packages/common/__tests__/components/Select.spec.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 
 import selectEvent from 'react-select-event'
+import { renderHook } from '@testing-library/react-hooks'
 
 import { fireEvent, render } from '@hub/test-utils'
 
+import { useTheme } from '../../layout'
 import '@testing-library/jest-dom'
 import Select, { PropsSelect } from '../../components/Select'
+
+jest.mock('../../layout', () => ({
+  useTheme: jest.fn(() => ({
+    colors: {
+      blue: {
+        400: 'DodgerBlue',
+        500: 'DeepSkyBlue',
+        600: 'LightSkyBlue',
+        700: 'SkyBlue'
+      }
+    },
+    shadows: {
+      sm: '1px;',
+      md: '2px;',
+      'dark-lg': '3px;'
+    }
+  }))
+}))
 
 const mockedProperties: PropsSelect = {
   variant: 'normal',
@@ -18,31 +38,50 @@ const mockedProperties: PropsSelect = {
 }
 
 describe('Select renders without crashing', () => {
+  const {
+    result: {
+      current: { colors }
+    }
+  } = renderHook(() => useTheme())
   it('Select normal variant render all options', async () => {
     const wrapper = render(<Select {...mockedProperties} />)
-    const { getAllByText, getByText } = wrapper
+    const { getAllByText, getByText, container } = wrapper
     selectEvent.openMenu(getByText('placeholder_test'))
     const options = getAllByText('option', { exact: false })
 
-    await selectEvent.select(getByText('placeholder_test'), 'option2')
-    // Evento de mousedown para alteracao no estilo com state.isSelected
-    fireEvent.mouseDown(getByText('option2'))
+    options.forEach(option => {
+      expect(option).not.toHaveStyle(`background-color: ${colors.blue[500]};`)
+    })
 
+    await selectEvent.select(getByText('placeholder_test'), 'option3')
+    // Evento de mousedown para alteracao no estilo com state.isSelected
+    fireEvent.mouseDown(getByText('option3'))
+    const optionSelected = container.getElementsByClassName('hub__option')[2]
+    expect(optionSelected).toHaveStyle(`background-color: ${colors.blue[500]};`)
     expect(options.length).toBe(3)
   })
 
   it('Select blue-transparent variant render options', async () => {
     mockedProperties.variant = 'blue-transparent'
     const wrapper = render(<Select {...mockedProperties} />)
-    const { getAllByText, getByText } = wrapper
+
+    const { getAllByText, getByText, container } = wrapper
 
     selectEvent.openMenu(getByText('placeholder_test'))
     const options = getAllByText('option', { exact: false })
+
+    options.forEach(option => {
+      expect(option).toHaveStyle(`background-color: ${colors.blue[500]};`)
+    })
 
     await selectEvent.select(getByText('placeholder_test'), 'option2')
     // Evento de mousedown para alteracao no estilo com state.isSelected
     fireEvent.mouseDown(getByText('option2'))
 
+    const option2 = 1
+    const selected = container.getElementsByClassName('hub__option')[option2]
+
+    expect(selected).toHaveStyle(`background-color: ${colors.blue[400]};`)
     expect(options.length).toBe(3)
   })
 
