9c662e5e495961a85e32643c5feec32824b9275c
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-02-24 10:36:20 -0300

    test(inicializando testes da página de login do hub): testes automatizados

--- packages/apps/hub-web/src/__tests__/pages/Auth/SignIn.spec.tsx
+++ packages/apps/hub-web/src/__tests__/pages/Auth/SignIn.spec.tsx
@@ -2,11 +2,71 @@ import React from 'react'
 
 import { render } from '@hub/test-utils'
 
-const Test = () => <h1>Test</h1>
+import SignIn from '~/pages/Auth/SignIn'
+import '@testing-library/jest-dom'
 
-describe('test init', () => {
-  it('test init', () => {
-    render(<Test />)
-    expect(1).toBe(1)
+jest.mock('react', () => {
+  const ui = jest.requireActual('react')
+  return {
+    ...ui,
+    useQuery: jest.fn(() => ({
+      get: jest.fn()
+    }))
+  }
+})
+
+jest.mock('react-router-dom', () => {
+  const rest = jest.requireActual('react-router-dom')
+  return {
+    ...rest,
+    useLocation: () => ({
+      pathname: '/login',
+      search: ''
+    }),
+    useHistory: () => ({
+      location: {
+        pathname: '/login'
+      },
+      push: jest.fn()
+    })
+  }
+})
+
+jest.mock('react-redux', () => {
+  const rest = jest.requireActual('react-redux')
+  return {
+    ...rest,
+    useDispatch: jest.fn(),
+    useSelector: jest.fn(() => ({
+      loading: false,
+      signInStrike: false
+    }))
+  }
+})
+
+describe('Testing that the Login page works correctly', () => {
+  beforeAll(() => {
+    process.env = Object.assign(process.env, {
+      REACT_APP_RECAPTCHA_SITE_KEY: '6LeT8ioaAAAAAIicz2pY2Q-opHa-MV45qsA9vZUX'
+    })
+  })
+
+  it('Should render the elements of the Login page', () => {
+    const { queryAllByText, queryByText, queryByPlaceholderText } = render(
+      <SignIn />
+    )
+    const config = { exact: false }
+    const signIn = queryAllByText('entrar', config)
+    const welcomeMessage = queryByText(
+      'Insira seus dados de acesso para começar',
+      config
+    )
+    const emailPlaceholder = queryByPlaceholderText('Digite seu usuário')
+    const passwordPlaceholder = queryByPlaceholderText('Digite sua senha')
+
+    expect(signIn.length).toBe(2)
+    expect(welcomeMessage).toBeInTheDocument()
+    expect(emailPlaceholder).toBeInTheDocument()
+    expect(passwordPlaceholder).toBeInTheDocument()
   })
 })
