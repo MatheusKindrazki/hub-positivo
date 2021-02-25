a77342610382a0ee0646d26a46bf33f1626708e1
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-02-25 15:49:35 -0300

    test(criando render with context): testes automatizados

--- packages/apps/hub-web/src/__tests__/pages/Auth/ExpiredToken.spec.tsx
+++ packages/apps/hub-web/src/__tests__/pages/Auth/ExpiredToken.spec.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 
+import * as redux from 'react-redux'
+
+import history from '~/services/history'
+
 import ExpiredToken from '~/pages/Auth/ExpiredToken'
 
 import { renderWithContext } from '~/hub-test-utils'
 
+jest.mock('~/services/history', () => ({
+  push: jest.fn()
+}))
+
 describe('Expired Token page should work properly', () => {
   it('debug', () => {
-    const wrapper = renderWithContext(<ExpiredToken />)
-    wrapper.debug()
+    // const spyPush = jest.spyOn(history, 'push')
+    // const wrapper = renderWithContext(<ExpiredToken />, {
+    //   forgotPassword: {
+    //     validateViewPin: true
+    //   }
+    // })
+    // const { getByText } = wrapper
+    // expect(spyPush).toHaveBeenCalledWith('a')
   })
 })
--- packages/apps/hub-web/src/hub-test-utils/renderWithContext.tsx
+++ packages/apps/hub-web/src/hub-test-utils/renderWithContext.tsx
@@ -2,15 +2,26 @@ import React, { ReactElement } from 'react'
 
 import { Provider } from 'react-redux'
 
+import createSagaMiddleware from 'redux-saga'
 import { PersistGate } from 'redux-persist/integration/react'
+import configureStore from 'redux-mock-store'
 
-import { store, persistor } from '~/store'
+import { persistor, store } from '~/store'
 
 import { render, RenderResult } from '@hub/test-utils'
 
-const renderWithContext = (children: ReactElement): RenderResult => {
+const sagaMiddleware = createSagaMiddleware()
+
+const middlewares = [sagaMiddleware]
+
+const mockStore = configureStore(middlewares)
+
+const renderWithContext = (
+  children: ReactElement,
+  INITIAL_STATE = {}
+): RenderResult => {
   return render(
-    <Provider store={store}>
+    <Provider store={mockStore({ ...store.getState(), ...INITIAL_STATE })}>
       <PersistGate persistor={persistor}>{children}</PersistGate>
     </Provider>
   )
