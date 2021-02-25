7aa76a693fadbdad52e92d1cd823f9c7303149b4
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-02-25 15:27:57 -0300

    chore: adicionando log para ver reconstrução do Iframe

--- packages/apps/hub-web/src/pages/Iframe/components/Iframe/index.tsx
+++ packages/apps/hub-web/src/pages/Iframe/components/Iframe/index.tsx
@@ -1,8 +1,12 @@
-import React, { createElement, IframeHTMLAttributes } from 'react'
+import React, { createElement, IframeHTMLAttributes, useEffect } from 'react'
 
 type IframeProps = IframeHTMLAttributes<HTMLIFrameElement>
 
 const Iframe: React.FC<IframeProps> = props => {
+  useEffect(() => {
+    console.log('reconstruindo iframe com a url:', props.src)
+  }, [props.src])
+
   return createElement('iframe', props)
 }
 
