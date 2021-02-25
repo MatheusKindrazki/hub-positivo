ea70643da4e01c779d506063c6ec008858068f46
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-02-25 15:19:03 -0300

    refactor: split de codigo no Iframe

--- packages/apps/hub-web/src/pages/Iframe/index.tsx
+++ packages/apps/hub-web/src/pages/Iframe/index.tsx
@@ -1,42 +1,21 @@
 import React, { useEffect, useState } from 'react'
 
-import { PulseLoader } from 'react-spinners'
-import { useParams } from 'react-router-dom'
-
 import { useDispatch, useSelector } from 'react-redux'
 
-import { preAuth } from '~/store/modules/authProduct/actions'
-import { store } from '~/store'
-
 import documentTitle from '@hub/common/utils/documentTitle'
-import createSlug from '@hub/common/utils/createSlug'
-import { useTheme } from '@hub/common/layout/styles'
-
-import history from '~/services/history'
 
 import postMessage from '~/middlewares/postMessage'
 
 import { IframeContainer } from './styles'
-import { getCardBySlug } from './services/getCardBySlug'
-
-interface IframePropsRouter {
-  solution: string
-  subpath: string
-}
-
+import getCardInformation from './hook/useCardInformation'
+import LoadingFrame from './components/Loading'
 const Iframe: React.FC = () => {
   postMessage()
 
   const dispatch = useDispatch()
 
-  const { colors } = useTheme()
   const [url, setUrl] = useState('')
   const [loading, setLoading] = useState(true)
-
-  const { guid } = store.getState().profile
-  const { level } = store.getState().educationalStage
-
-  const { solution, subpath } = useParams<IframePropsRouter>()
 
   const { frameUrl, frameName } = useSelector(
     (state: Store.State) => state.products
@@ -47,49 +26,14 @@ const Iframe: React.FC = () => {
       getCardInformation()
       return
     }
-    async function getCardInformation() {
-      const card = await getCardBySlug({
-        slug: solution,
-        nivelEnsino: level,
-        perfil: guid
-      })
-      if (!card) return history.push('/')
-      const product = createSlug(card.nome)
 
-      let path = ''
-      if (subpath) {
-        const queryParams = window.location.hash.split('?')[1] || undefined
-
-        path = `${subpath}${queryParams ? `?${queryParams}` : ''}`
-      }
-
-      dispatch(
-        preAuth({
-          name: card.nome,
-          url: card.link || '',
-          tipoRenderizacao: card.tipoRenderizacao,
-          product: product,
-          subpath: path.includes('undefined') ? '' : path
-        })
-      )
-    }
     documentTitle(frameName || 'Hub')
     return setUrl(frameUrl || '')
-  }, [dispatch, frameName, frameUrl, guid, level, solution, subpath])
+  }, [dispatch, frameName, frameUrl])
 
   return (
     <IframeContainer>
-      <PulseLoader
-        color={colors.blue[500]}
-        loading={loading}
-        size={30}
-        css={`
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translateX(-50%);
-        `}
-      />
+      <LoadingFrame loading={loading} />
       <iframe
         id="hub-solution-iframe"
         loading="lazy"
