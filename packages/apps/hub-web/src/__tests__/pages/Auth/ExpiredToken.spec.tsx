9fb634467f6d91963ecfcf202e8d61ba15bdc1d7
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-01 17:22:52 -0300

    test(testando página de token expirado do hub): testes automatizados

--- packages/apps/hub-web/src/__tests__/pages/Auth/ExpiredToken.spec.tsx
+++ packages/apps/hub-web/src/__tests__/pages/Auth/ExpiredToken.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { store } from '~/store'
 
-import { render, CustomState } from '@hub/test-utils'
+import { render, CustomState, fireEvent } from '@hub/test-utils'
 
 import history from '~/services/history'
 
@@ -13,19 +13,77 @@ jest.mock('~/services/history', () => ({
 }))
 
 describe('Expired Token page should work properly', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   const CUSTOM_STATE: CustomState = {
     forgotPassword: {
-      validateViewPin: false
+      validateViewPin: true
     }
   }
-  it('debug', () => {
-    const spyPush = jest.spyOn(history, 'push')
+
+  const spyPush = jest.spyOn(history, 'push')
+
+  it('Should redirect to `/` when `validateViewPin` doesn`t exist or it`s false', () => {
     render(<ExpiredToken />, {
+      store,
+      reducers: ['forgotPassword']
+    })
+
+    expect(spyPush).toHaveBeenCalledTimes(1)
+    expect(spyPush).toHaveBeenCalledWith('/')
+  })
+
+  it('Should render the correcly elements on screen', () => {
+    const { queryByText } = render(<ExpiredToken />, {
       store,
       reducers: ['forgotPassword'],
       CUSTOM_STATE
     })
-    // const { getByText } = wrapper
-    expect(spyPush).toHaveBeenCalledWith('/')
+
+    const message = queryByText(
+      /Solicite um novo link para redefinir sua senha./i
+    )
+    const expiredLinkTitle = queryByText(/O link expirou/i)
+    const newLinkButton = queryByText(/Solicitar novo link/i)
+
+    expect(message).toBeInTheDocument()
+    expect(expiredLinkTitle).toBeInTheDocument()
+    expect(newLinkButton).toBeInTheDocument()
+  })
+
+  it('Should redirect to `/esqueci-minha-senha` when `Solicitar novo link` is clicked', () => {
+    const wrapper = render(<ExpiredToken />, {
+      store,
+      reducers: ['forgotPassword'],
+      CUSTOM_STATE
+    })
+    const { getByText } = wrapper
+    const newLinkButton = getByText(/Solicitar novo link/i)
+
+    expect(newLinkButton).toBeInTheDocument()
+
+    fireEvent.click(newLinkButton)
+
+    expect(spyPush).toHaveBeenCalledTimes(1)
+    expect(spyPush).toHaveBeenCalledWith('/esqueci-minha-senha')
+  })
+
+  it('Should redirect to `/login` when `O link expirou` is clicked', () => {
+    const wrapper = render(<ExpiredToken />, {
+      store,
+      reducers: ['forgotPassword'],
+      CUSTOM_STATE
+    })
+    const { getByTestId } = wrapper
+    const goBackButton = getByTestId('go-back')
+
+    expect(goBackButton).toBeInTheDocument()
+
+    fireEvent.click(goBackButton)
+
+    expect(spyPush).toHaveBeenCalledTimes(1)
+    expect(spyPush).toHaveBeenCalledWith('/login')
   })
 })
