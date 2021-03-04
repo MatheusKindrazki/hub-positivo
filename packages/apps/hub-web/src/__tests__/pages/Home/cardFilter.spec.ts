e7b05d510b674a14953d6fff95a3cdeec7ac2ffa
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-04 10:31:27 -0300

    test(inicializando testes do filtro do hub): testes automatizados

---
+++ packages/apps/hub-web/src/__tests__/pages/Home/cardFilter.spec.ts
@@ -0,0 +1,9 @@
+import { cardFilter } from '~/pages/Home/cardFilter'
+
+import { products } from './mockState.json'
+
+describe('testing cardFilter functions', () => {
+  it('getting started', () => {
+    const { data } = products
+  })
+})
