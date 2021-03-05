1e8ed9ec81ef5d4a67877e766ff78d999708086b
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-05 13:51:09 -0300

    fix(fix no teste do componente commom do popover/inicializar testes do cardfilter de utils): fix

---
+++ packages/apps/hub-web/src/__tests__/utils/cardFilter.spec.ts
@@ -0,0 +1,8 @@
+import { cardFilter } from '~/utils/cardFilter'
+
+describe('testing cardFilter funcionalities', () => {
+  it('getting started', () => {
+    console.log(cardFilter)
+    expect(1).toBe(1)
+  })
+})
