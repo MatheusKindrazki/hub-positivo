da5f34fce1b16e735b24056826fc81280f8072a6
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-08 09:49:49 -0300

    test(refatorar forgotfail.spec para utilizar setup() de testes): testes automatizados

--- packages/apps/hub-web/src/__tests__/pages/Auth/ForgotFail.spec.tsx
+++ packages/apps/hub-web/src/__tests__/pages/Auth/ForgotFail.spec.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import * as reactDom from 'react-router-dom'
-import {} from 'react-router-dom'
 
 import { store } from '~/store'
 
@@ -43,22 +42,24 @@ describe('Expired Token page should work properly', () => {
     }
   }
 
-  it('Should redirect to `/login` when `sendViewToken` doesn`t exist or it`s false', () => {
-    render(<ForgotFail />, {
+  const setup = (rest: CustomState | object) => {
+    const utils = render(<ForgotFail />, {
       store,
-      reducers: ['forgotPassword']
+      reducers: ['forgotPassword'],
+      CUSTOM_STATE: rest
     })
+    return { ...utils }
+  }
+
+  it('Should redirect to `/login` when `sendViewToken` doesn`t exist or it`s false', () => {
+    setup({})
 
     expect(push).toHaveBeenCalledTimes(1)
     expect(push).toHaveBeenCalledWith('/login')
   })
 
   it('Should render the correcly elements on screen', () => {
-    const { queryByText } = render(<ForgotFail />, {
-      store,
-      reducers: ['forgotPassword'],
-      CUSTOM_STATE
-    })
+    const { queryByText } = setup(CUSTOM_STATE)
 
     const failLinkTitle = queryByText(/O link não pôde ser enviado/i)
     const message = queryByText(
@@ -72,12 +73,8 @@ describe('Expired Token page should work properly', () => {
   })
 
   it('Should redirect to `/login` when `Voltar para o login` is clicked', () => {
-    const wrapper = render(<ForgotFail />, {
-      store,
-      reducers: ['forgotPassword'],
-      CUSTOM_STATE
-    })
-    const { getByText } = wrapper
+    const { getByText } = setup(CUSTOM_STATE)
+
     const goBackToLogin = getByText(/Voltar para o login/i)
 
     expect(goBackToLogin).toBeInTheDocument()
@@ -89,12 +86,8 @@ describe('Expired Token page should work properly', () => {
   })
 
   it('Should redirect to `/login` when `GoBack` is clicked', () => {
-    const wrapper = render(<ForgotFail />, {
-      store,
-      reducers: ['forgotPassword'],
-      CUSTOM_STATE
-    })
-    const { getByTestId } = wrapper
+    const { getByTestId } = setup(CUSTOM_STATE)
+
     const goBackButton = getByTestId('go-back')
 
     expect(goBackButton).toBeInTheDocument()
