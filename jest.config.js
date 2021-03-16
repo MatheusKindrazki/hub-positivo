71cfa82121d2ce66d9aa8f5dc44932336a0fb2e8
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-03-16 14:56:15 -0300

    chore: reduzindo regua de aceite jest coverage

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,10 @@ module.exports = {
   coverageReporters: ['text', 'lcov', 'json-summary'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
     }
   },
   globals: {
