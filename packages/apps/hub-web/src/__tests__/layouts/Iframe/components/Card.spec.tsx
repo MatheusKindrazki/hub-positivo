bd551bfef2668c08079d1b4a11c444877016278b
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-16 17:18:10 -0300

    test(criar setup de testes para layout:iframe/components/card): testes automatizados

--- packages/apps/hub-web/src/__tests__/layouts/Iframe/components/Card.spec.tsx
+++ packages/apps/hub-web/src/__tests__/layouts/Iframe/components/Card.spec.tsx
@@ -1,7 +1,32 @@
-import Card from '~/layouts/Iframe/components/Card'
+import React from 'react'
+
+import { render } from '@hub/test-utils'
+
+import Card, { CardProps } from '~/layouts/Iframe/components/Card'
 
 describe('getting started', () => {
+  let card: CardProps
+
+  beforeAll(() => {
+    card = {
+      id: 'id',
+      nome: 'card name',
+      descricao: 'description',
+      cor: 'blue',
+      arquivo: 'file',
+      notificacao: 'notification',
+      ativo: true,
+      link: 'psd.link.com',
+      tipoRenderizacao: 'iframe'
+    }
+  })
+  const setup = (card = {} as CardProps) => {
+    const onClick = jest.fn()
+    const wrapper = render(<Card card={card} onClick={onClick} />)
+    return { ...wrapper, onClick }
+  }
   it('it', () => {
-    expect(1).toBe(1)
+    const { debug } = setup(card)
+    debug()
   })
 })
