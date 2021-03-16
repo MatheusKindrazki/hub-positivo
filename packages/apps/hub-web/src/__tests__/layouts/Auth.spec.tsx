5b12edf44b70a27d71ac101415bb6fcf4c2ed863
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-16 14:04:25 -0300

    test(finalizando testes do layout:auth): testes automatizados

--- packages/apps/hub-web/src/__tests__/layouts/Auth.spec.tsx
+++ packages/apps/hub-web/src/__tests__/layouts/Auth.spec.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 
+import { store } from '~/store'
+
 import { render } from '@hub/test-utils'
 
 import Auth from '~/layouts/Auth'
 
-jest.mock('react-redux', () => ({
-  useSelector: jest.fn(() => ({ loading: false }))
-}))
-
 describe('Auth`s layout should render without crashing', () => {
   it('Auth`s layout should encapsulate correctly the children', () => {
     const element = 'children'
-    const { getByText, debug } = render(<Auth>{element}</Auth>)
+    process.env.REACT_APP_VERSION = '1.0'
+    const { getByText, debug } = render(<Auth>{element}</Auth>, {
+      store,
+      reducers: ['global']
+    })
     const children = getByText(element)
+    const version = getByText(/1.0/)
 
     expect(children).toBeInTheDocument()
+    expect(version).toBeInTheDocument()
     debug()
   })
 })
