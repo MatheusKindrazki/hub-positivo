0a7c6de266b3452e9ef2f19f7e5113c6ea0967b5
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-18 14:48:43 -0300

    test(finalizando testes do layout do header): testes automatizados

--- packages/apps/hub-web/src/__tests__/layouts/Iframe/components/Header/Header.spec.tsx
+++ packages/apps/hub-web/src/__tests__/layouts/Iframe/components/Header/Header.spec.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 
-import { render } from '@hub/test-utils'
+import { fireEvent, render } from '@hub/test-utils'
 
-import Header from '~/layouts/Iframe/components/Header'
+import history from '~/services/history'
 
-jest.mock('~/layouts/Iframe/components/Header/HeaderDesktop', () =>
-  jest.fn(() => <span>Header Desktop</span>)
-)
-
-jest.mock('~/layouts/Iframe/components/Header/HeaderMobile', () =>
-  jest.fn(() => <span>Header Mobile</span>)
-)
+import * as HeaderMobile from '~/layouts/Iframe/components/Header/HeaderMobile'
+import * as HeaderDesktop from '~/layouts/Iframe/components/Header/HeaderDesktop'
+import * as AnimateGoBack from '~/layouts/Iframe/components/Header/AnimateGoBack'
+import Header from '~/layouts/Iframe/components/Header/'
 
 jest.mock('@hub/common/layout/styles', () => {
   const rest = jest.requireActual('@hub/common/layout/styles')
@@ -18,28 +15,64 @@ jest.mock('@hub/common/layout/styles', () => {
     ...rest,
     useMediaQuery: jest
       .fn()
-      .mockReturnValueOnce([true])
       .mockReturnValueOnce([false])
+      .mockReturnValue([true])
   }
 })
 
-const setup = () => {
-  const handlePush = jest.fn()
+jest.mock('~/services/history', () => ({
+  push: jest.fn()
+}))
 
-  const wrapper = render(<Header handlePush={handlePush} />)
-  return { ...wrapper, handlePush }
-}
+jest.mock('~/layouts/Iframe/components/Header/HeaderMobile', () => jest.fn())
+jest.mock('~/layouts/Iframe/components/Header/HeaderDesktop', () => jest.fn())
+
+jest.mock('~/layouts/Iframe/components/Header/AnimateGoBack', () =>
+  jest.fn(() => <></>)
+)
 
 describe('Header`s layout should work properly', () => {
-  it('Header should render Desktop Header when useMediaQuery returns [true]', () => {
+  const setup = () => {
+    const wrapper = render(<Header handlePush={jest.fn()} cards={[]} />)
+    return { ...wrapper }
+  }
+  it('Header should render Mobile Header when useMediaQuery returns [false]', () => {
+    const mobile = 'Header Mobile'
+
+    jest
+      .spyOn(HeaderMobile, 'default')
+      .mockImplementation(jest.fn(() => <span>{mobile}</span>))
+
     const { getByText } = setup()
-    const headerMobile = getByText('Header Desktop')
-    expect(headerMobile).toBeInTheDocument()
+    const Header = getByText(mobile)
+    expect(Header).toBeInTheDocument()
+  })
+  it('Header should render Desktop Header when useMediaQuery returns [true]', () => {
+    const desktop = 'Header Desktop'
+
+    jest
+      .spyOn(HeaderDesktop, 'default')
+      .mockImplementation(jest.fn(() => <span>{desktop}</span>))
+
+    const { getByText } = setup()
+    const Header = getByText(desktop)
+    expect(Header).toBeInTheDocument()
   })
 
-  it('Header should render Mobile Header when useMediaQuery returns [false]', () => {
-    const { getByText } = setup()
-    const desktopHeader = getByText('Header Mobile')
-    expect(desktopHeader).toBeInTheDocument()
+  it('Should call the go back handler when a GoBack is clicked', () => {
+    const goBack = 'Go Back'
+    jest
+      .spyOn(AnimateGoBack, 'default')
+      .mockImplementation(
+        jest.fn(({ onClick }) => <button onClick={onClick}>{goBack}</button>)
+      )
+    const spyPush = jest.spyOn(history, 'push')
+
+    const { getAllByRole } = setup()
+    const headerButtons = getAllByRole('button')
+    const goBackButton = headerButtons[0]
+
+    fireEvent.click(goBackButton)
+    expect(spyPush).toHaveBeenLastCalledWith('/')
   })
 })
