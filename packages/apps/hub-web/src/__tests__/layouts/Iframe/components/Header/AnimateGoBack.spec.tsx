ab9df0109b6d6b24ffc7c7a3750671cb7e7c0304
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-18 16:59:32 -0300

    test(boilerplate do teste de layout: animate go back): testes automatizados

--- packages/apps/hub-web/src/__tests__/layouts/Iframe/components/Header/AnimateGoBack.spec.tsx
+++ packages/apps/hub-web/src/__tests__/layouts/Iframe/components/Header/AnimateGoBack.spec.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 
-import { render } from '@hub/test-utils'
+import { fireEvent, render, waitFor } from '@hub/test-utils'
 
 import AnimateGoBack from '~/layouts/Iframe/components/Header/AnimateGoBack'
 
+// width: 60 : 40
+
 describe('getting started', () => {
-  const setup = () => {
+  const setup = (width: number) => {
     const onClick = jest.fn()
-    const wrapper = render(<AnimateGoBack onClick={onClick} />)
+    const wrapper = render(<AnimateGoBack onClick={onClick} width={width} />)
     return { ...wrapper, onClick }
   }
-  it('it', () => {
-    const { getByTestId } = setup()
+  it('it', async () => {
+    jest.useFakeTimers()
+    const { getByTestId, findAllByRole } = setup(60)
+    const headerButtons = await findAllByRole('button')
+    const hubLogo = getByTestId('hub-logo-testid')
+
+    const arrowLeft = headerButtons[0]
+
+    expect(arrowLeft).not.toBeVisible()
+
+    fireEvent.mouseDown(hubLogo)
+
+    const test = await findAllByRole('button')
+    await waitFor(() => expect(test[0]).toBeVisible())
   })
 })
