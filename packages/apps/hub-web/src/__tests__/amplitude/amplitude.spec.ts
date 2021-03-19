867d7f14f9a069625cc2caa7830259481873e579
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-19 12:30:45 -0300

    test(amplitude.spec.tsx): testes automatizados
    inicialiar testes automatizados das features do amplitude: amplitudeInit

--- packages/apps/hub-web/src/__tests__/amplitude/amplitude.spec.ts
+++ packages/apps/hub-web/src/__tests__/amplitude/amplitude.spec.ts
@@ -1,19 +1,6 @@
 import amplitude from 'amplitude-js'
-import { renderHook } from '@testing-library/react-hooks'
 
-import * as redux from 'react-redux'
-
-import { store } from '~/store'
-
-import {
-  amplitudeToolOpened,
-  EventData
-} from '~/services/amplitude/amplitudeToolOpened'
 import { amplitudeInit } from '~/services/amplitude/amplitudeInit'
-
-// import { useAmplitudePageView } from '~/hooks/amplitude/useAmplitudePageView'
-
-import { useAmplitudeSetProperties } from '../../hooks/amplitude/useAmplitudeSetProperties'
 
 const mockState = {
   profile: {
@@ -39,19 +26,6 @@ const mockState = {
   }
 }
 
-const mockParamsSetProperties = {
-  educational_stages: ['Fake level (fake value)', 'Fake level (fake value)'],
-  roles: ['fakeRole', 'fakeRole'],
-  selectedRole: mockState.profile.name,
-  school: mockState.user.school,
-  user: mockState.user.user
-}
-
-const mockedEventProperties: EventData = {
-  card_name: 'teste',
-  location: 'dashboard'
-}
-
 jest.mock('amplitude-js', () => ({
   getInstance: jest.fn().mockReturnValue({
     init: jest.fn(),
@@ -73,7 +47,7 @@ jest.mock('react-router-dom', () => ({
 
 describe('testing amplitude functions', () => {
   const instance = amplitude.getInstance()
-  const { init, logEvent, setUserId, setUserProperties } = instance
+  const { init } = instance
 
   afterEach(() => {
     jest.clearAllMocks()
@@ -82,21 +56,5 @@ describe('testing amplitude functions', () => {
   it('Amplitude should initialize on amplitudeInit', () => {
     amplitudeInit()
     expect(init).toHaveBeenCalledTimes(1)
-  })
-
-  it('amplitudeToolOpened send event with correct properties', () => {
-    amplitudeToolOpened(mockedEventProperties)
-    expect(logEvent).toHaveBeenCalledWith('Tool Opened', mockedEventProperties)
-  })
-
-  it.skip('useAmplitudePageView should log event', () => {
-    // renderHook(() => useAmplitudePageView({ isPrivate: true }))
-    // expect(logEvent).toHaveBeenCalled()
-  })
-
-  it.skip('useAmplitudePageView shouldn`t log event when signed is false', () => {
-    mockState.auth.signed = false
-    // renderHook(() => useAmplitudePageView({ isPrivate: true }))
-    // expect(logEvent).not.toHaveBeenCalled()
   })
 })
