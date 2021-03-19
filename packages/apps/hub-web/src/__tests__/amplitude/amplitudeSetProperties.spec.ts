7e110af2c8ca46d2ae998b32c90d535ba864d53d
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-19 17:16:48 -0300

    test: molde para testes do amplitude: setproperties e setId

--- packages/apps/hub-web/src/__tests__/amplitude/amplitudeSetProperties.spec.ts
+++ packages/apps/hub-web/src/__tests__/amplitude/amplitudeSetProperties.spec.ts
@@ -1,18 +1,18 @@
 import amplitude from 'amplitude-js'
 
-import { amplitudeInit } from '~/services/amplitude/amplitudeInit'
+// import { amplitudeInit } from '~/services/amplitude/amplitudeInit'
 
-jest.mock('amplitude-js', () => ({
-  getInstance: jest.fn().mockReturnValue({
-    init: jest.fn()
-  })
-}))
+// jest.mock('amplitude-js', () => ({
+//   getInstance: jest.fn().mockReturnValue({
+//     init: jest.fn()
+//   })
+// }))
 
 describe('testing if amplitude set properties functions work properly', () => {
-  const instance = amplitude.getInstance()
-  const { init } = instance
-  it('Amplitude should initialize on amplitudeInit', () => {
-    amplitudeInit()
-    expect(init).toHaveBeenCalledTimes(1)
+  // const instance = amplitude.getInstance()
+  // const { init } = instance
+
+  it('get started', () => {
+    expect(1).toBe(1)
   })
 })
