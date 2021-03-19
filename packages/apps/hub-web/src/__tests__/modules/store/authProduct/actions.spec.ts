ffd2156c1b61da928925365d84951b8bd6ee7b5f
gabryelryba <gabryel.ryba@gmail.com>
2021-03-19 14:36:30 -0300

    test(authproduct): testes dos action creators do modulo authProduct da store

---
+++ packages/apps/hub-web/src/__tests__/modules/store/authProduct/actions.spec.ts
@@ -0,0 +1,51 @@
+import {
+  preAuth,
+  authProductRequest,
+  authProductSuccess,
+  authProductFailure,
+  Actions as types
+} from '~/store/modules/authProduct/actions'
+
+describe('authProduct action creators should work properly', () => {
+  it('should create an request action on preAuth', () => {
+    const payload = {
+      product: 'string',
+      name: 'string',
+      url: 'string',
+      tipoRenderizacao: 'string',
+      subpath: 'string'
+    }
+    const expectedAction = {
+      type: types.AUTH_PRODUCT_REQUEST,
+      payload
+    }
+    expect(preAuth(payload)).toEqual(expectedAction)
+  })
+
+  it('should create an specific request action on authProductRequest', () => {
+    const payload = {
+      product: 'string',
+      name: 'string',
+      url: 'string',
+      tipoRenderizacao: 'string',
+      subpath: 'string'
+    }
+    const expectedAction = {
+      type: types.AUTH_PRODUCT_EEM_REQUEST,
+      payload
+    }
+    expect(authProductRequest(payload, 'AUTH_PRODUCT_EEM_REQUEST')).toEqual(
+      expectedAction
+    )
+  })
+
+  it('should create an success action on authProductSuccess', () => {
+    const expectedAction = { type: types.AUTH_PRODUCT_SUCCESS }
+    expect(authProductSuccess()).toEqual(expectedAction)
+  })
+
+  it('should create an failure action on authProductFailure', () => {
+    const expectedAction = { type: types.AUTH_PRODUCT_FAILURE }
+    expect(authProductFailure()).toEqual(expectedAction)
+  })
+})
