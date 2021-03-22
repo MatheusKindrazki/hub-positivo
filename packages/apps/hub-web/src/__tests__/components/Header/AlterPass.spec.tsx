79f23c24d6816a331ec6e38e9c90744a633ee5b6
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-22 11:04:48 -0300

    feat(alterpass.spec.tsx): testes automatizados
    criando setup de testes para renderização correta do componente a ser testado

--- packages/apps/hub-web/src/__tests__/components/Header/AlterPass.spec.tsx
+++ packages/apps/hub-web/src/__tests__/components/Header/AlterPass.spec.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-import { render } from '@hub/test-utils'
+import { store } from '~/store'
+
+import { render, CustomState } from '@hub/test-utils'
+
+import AlterPass from '~/components/Header/components/AlterPass'
 
 describe('get started', () => {
+  afterEach(() => jest.clearAllMocks())
+
+  const setup = (CUSTOM_STATE = {} as CustomState) => {
+    const onClose = jest.fn()
+    const wrapper = render(<AlterPass onClose={onClose} />, {
+      store,
+      reducers: ['user'],
+      CUSTOM_STATE
+    })
+    return { ...wrapper, onClose }
+  }
   it('it', () => {
-    expect(1).toBe(1)
+    const { debug } = setup()
+    debug()
   })
 })
