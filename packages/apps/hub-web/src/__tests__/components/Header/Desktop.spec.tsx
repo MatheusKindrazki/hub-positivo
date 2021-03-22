c10376494322b1ba5ff6cc7e66b192ae2c1a96a8
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-22 14:24:09 -0300

    feat(desktop.spec.tsx): testes automatizados
    criando mock para useHeader()

--- packages/apps/hub-web/src/__tests__/components/Header/Desktop.spec.tsx
+++ packages/apps/hub-web/src/__tests__/components/Header/Desktop.spec.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
 
-import { render } from '@hub/test-utils'
+import { store } from '~/store'
+
+import { render, CustomState } from '@hub/test-utils'
+
+import { ContextHeaderProps } from '~/components/Header/context/types'
+import Desktop from '~/components/Header/components/Desktop'
+
+jest.mock('~/components/Header/context', () => ({
+  useHeader: jest.fn().mockReturnValue({
+    schoolList: [],
+    roleList: [],
+    setSchool: jest.fn(),
+    setRole: jest.fn(),
+    resetInfo: jest.fn(),
+    defaultValue: {
+      school: { value: 'value', label: 'label' },
+      role: { value: 'value', label: 'label' }
+    }
+  } as ContextHeaderProps)
+}))
 
 describe('get started', () => {
+  const setup = (CUSTOM_STATE = {} as CustomState) => {
+    const openModalPass = jest.fn()
+    const wrapper = render(<Desktop openModalPass={openModalPass} />, {
+      reducers: ['tour', 'user', 'profile'],
+      store,
+      CUSTOM_STATE
+    })
+    return { ...wrapper }
+  }
   it('it', () => {
-    expect(1).toBe(1)
+    const wrapper = setup()
   })
 })
