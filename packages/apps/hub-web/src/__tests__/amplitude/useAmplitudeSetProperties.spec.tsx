6340ee4a8838f50d231a4dfe109ea6646fc1fc71
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-22 09:12:28 -0300

    feat: testes automatizados
    criando contexto para renderizar o hook do amplitude e criando mock para useSelector

---
+++ packages/apps/hub-web/src/__tests__/amplitude/useAmplitudeSetProperties.spec.tsx
@@ -0,0 +1,94 @@
+import React from 'react'
+
+import amplitude from 'amplitude-js'
+import { renderHook } from '@testing-library/react-hooks'
+
+import { Provider } from 'react-redux'
+
+import { store } from '~/store'
+
+import { useAmplitudeSetProperties } from '~/hooks/amplitude/useAmplitudeSetProperties'
+
+jest.mock('amplitude-js', () => ({
+  getInstance: jest.fn().mockReturnValue({
+    setUserProperties: jest.fn()
+  })
+}))
+
+const state = {
+  user: {
+    guid: 'guid',
+    username: 'username',
+    name: 'name',
+    schools: [
+      {
+        id: 'user_school_id',
+        name: 'user_school_name',
+        roles: ['Administrador']
+      }
+    ]
+  },
+  school: {
+    value: 'school_value',
+    label: 'school_label',
+    user_id: 'school_user_id',
+    integration_id: 'school_integradion_id',
+    roles: ['Professor', 'Administrador']
+  },
+  profiles: [
+    {
+      id: 'profile_id',
+      name: 'profile_name',
+      icon: 'profile_icon',
+      colorProfile: 'profile_color'
+    }
+  ],
+  name: 'selected_role',
+  levels: [{ value: 'EF1', label: 'educational_stage_EF1' }],
+  class: 'selected_class',
+  level: 'level'
+}
+
+jest.mock('react-redux', () => {
+  const rest = jest.requireActual('react-redux')
+  return {
+    ...rest,
+    useSelector: jest.fn(() => state)
+  }
+})
+
+const wrapper: React.FC = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>
+}
+
+describe('testing if amplitude set properties functions work properly', () => {
+  const instance = amplitude.getInstance()
+  const { setUserProperties } = instance
+
+  it('Should call setUserProperties with the correct informations', async () => {
+    renderHook(() => useAmplitudeSetProperties(), { wrapper })
+
+    expect(setUserProperties).toHaveBeenCalledWith({
+      educational_stage_EF1: true,
+      educational_stage_EF2: false,
+      educational_stage_EI: false,
+      educational_stage_EM: false,
+      is_admin: false,
+      is_coordinator: false,
+      is_family: false,
+      is_student: false,
+      is_teacher: false,
+      roles_list: ['profile_name'],
+      schools_list: ['user_school_name'],
+      selectedRole: 'selected_role',
+      selected_class: 'selected_class',
+      selected_educational_stage: 'level',
+      selected_school_id: 'school_value',
+      selected_school_name: 'school_label',
+      selected_school_sge: null,
+      user_id: 'guid',
+      user_login: 'username',
+      user_name: 'name'
+    })
+  })
+})
