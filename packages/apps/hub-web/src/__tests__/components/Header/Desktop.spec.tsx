61cc2714b4c7f26d8d0a9086867cfc99c694cb51
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-23 13:11:37 -0300

    feat: testes automatizados
    testando elementos do Header component do hub

--- packages/apps/hub-web/src/__tests__/components/Header/Desktop.spec.tsx
+++ packages/apps/hub-web/src/__tests__/components/Header/Desktop.spec.tsx
@@ -3,85 +3,70 @@ import React from 'react'
 import { store } from '~/store'
 
 import { render, CustomState, fireEvent, waitFor } from '@hub/test-utils'
-import * as components from '@hub/common/components'
+
+import history from '~/services/history'
 
 import { ContextHeaderProps } from '~/components/Header/context/types'
 import * as header from '~/components/Header/context'
 import Desktop from '~/components/Header/components/Desktop'
 
+jest.mock('~/services/history', () => ({
+  push: jest.fn()
+}))
+
 jest.mock('~/components/Header/context', () => {
   const rest = jest.requireActual('~/components/Header/context')
   return {
     ...rest,
-    useHeader: jest.fn().mockReturnValue({
-      schoolList: [
-        {
-          label: 'school_list_label',
-          value: 'school_list_value',
-          roles: ['school_list_roles']
-        }
-      ],
-      roleList: [
-        {
-          name: 'role_name',
-          icon: 'role_icon',
-          colorProfile: 'role_color_profile',
-          id: 'role_id',
-          label: 'role_label',
-          value: 'role_value'
-        }
-      ],
-      setSchool: jest.fn(),
-      setRole: jest.fn(),
-      resetInfo: jest.fn(),
-      defaultValue: {
-        school: {
-          value: 'default_school_value',
-          label: 'default_school_label'
-        },
-        role: { value: 'default_role_value', label: 'default_role_label' }
-      }
-    } as ContextHeaderProps)
+    useHeader: jest.fn()
   }
 })
 
-describe('get started', () => {
-  const useHeaderReturn = {
-    schoolList: [
-      {
-        label: 'school_list_label',
-        value: 'school_list_value',
-        roles: ['school_list_roles']
-      }
-    ],
-    roleList: [
-      {
-        name: 'role_name',
-        icon: 'role_icon',
-        colorProfile: 'role_color_profile',
-        id: 'role_id',
-        label: 'role_label',
-        value: 'role_value'
-      }
-    ],
-    setSchool: jest.fn(),
-    setRole: jest.fn(),
-    resetInfo: jest.fn(),
-    defaultValue: {
-      school: {
-        value: 'default_school_value',
-        label: 'default_school_label'
-      },
-      role: { value: 'default_role_value', label: 'default_role_label' }
+const useHeaderReturn = {
+  schoolList: [
+    {
+      label: 'school_list_label',
+      value: 'school_list_value',
+      roles: ['school_list_roles']
     }
-  } as ContextHeaderProps
+  ],
+  roleList: [
+    {
+      name: 'role_name',
+      icon: 'role_icon',
+      colorProfile: 'role_color_profile',
+      id: 'role_id',
+      label: 'role_label',
+      value: 'role_value'
+    }
+  ],
+  setSchool: jest.fn(),
+  setRole: jest.fn(),
+  resetInfo: jest.fn(),
+  defaultValue: {
+    school: {
+      value: 'default_school_value',
+      label: 'default_school_label'
+    },
+    role: { value: 'default_role_value', label: 'default_role_label' }
+  }
+} as ContextHeaderProps
+
+describe('get started', () => {
+  jest.spyOn(header, 'useHeader').mockReturnValue(useHeaderReturn)
 
   const { defaultValue } = useHeaderReturn
-  const spyUseHeader = jest
-    .spyOn(header, 'useHeader')
-    .mockReturnValue(useHeaderReturn)
 
-  const setup = (CUSTOM_STATE = {} as CustomState) => {
+  const name = 'Fake Name'
+  const user = {
+    user: {
+      user: {
+        name
+      }
+    }
+  }
+
+  const setup = (CUSTOM_STATE = { ...user } as CustomState) => {
     const openModalPass = jest.fn()
     const wrapper = render(<Desktop openModalPass={openModalPass} />, {
       reducers: ['tour', 'user', 'profile'],
@@ -89,8 +74,9 @@ describe('get started', () => {
       CUSTOM_STATE
     })
 
+    const popOverTrigger = wrapper.getByTestId('popover-trigger')
     wrapper.storeUtils?.clearActions()
-    return { ...wrapper }
+    return { ...wrapper, popOverTrigger }
   }
   it('Should dispatch a `@tour/OPEN_TOUR` action when `Fazer tour` button is clicked', () => {
     const { getByText, storeUtils } = setup({
@@ -118,39 +104,42 @@ describe('get started', () => {
     ])
   })
 
-  it.only('Should open popover when header`s avatar is clicked ', async () => {
-    jest.useFakeTimers()
-    const name = 'Fake Name'
-    const { getByText, getByTestId, debug } = setup({
-      user: {
-        user: {
-          name
-        }
-      }
-    })
-    const popOverTrigger = getByTestId('popover-trigger')
+  it('Popover should be visible when header`s avatar is clicked ', async () => {
+    const { getByTestId, popOverTrigger } = setup()
     const popOverContent = getByTestId('popover-content')
 
     expect(popOverContent).not.toBeVisible()
-
     expect(popOverTrigger).toBeInTheDocument()
-    await waitFor(() => fireEvent.click(popOverTrigger))
-    jest.runAllTimers()
 
-    // debug()
-
-    // expect(popOverContent).toBeVisible()
+    fireEvent.click(popOverTrigger)
+    await waitFor(() => expect(popOverContent).toBeVisible())
   })
 
   it.skip('should call `onOpen` function when `Estou com uma dúvida` is clicked', () => {
     const onClick = jest.fn()
     jest
-      .spyOn(components, 'Button')
+      .spyOn('components', 'Button')
       .mockImplementation(() => <button onClick={onClick}>TESTE</button>)
     const { getByText, debug } = setup()
     const help = getByText(/Estou com uma dúvida/i)
 
     fireEvent.click(help)
     debug()
+  })
+
+  it('should dispatch an `@auth/SIGN_OUT` action when `Sair` is clicked', async () => {
+    const spyPush = jest.spyOn(history, 'push')
+    jest.useFakeTimers()
+    const { findByText, storeUtils, popOverTrigger } = setup()
+
+    fireEvent.click(popOverTrigger)
+    const exit = await findByText(/Sair/i)
+
+    fireEvent.click(exit)
+    jest.runAllTimers()
+
+    const action = storeUtils?.getActions()
+    expect(action).toStrictEqual([{ type: '@auth/SIGN_OUT' }])
+    expect(spyPush).toHaveBeenCalledWith('/login')
   })
 })
