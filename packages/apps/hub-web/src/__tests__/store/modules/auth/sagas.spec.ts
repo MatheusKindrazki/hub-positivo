ffd7be36873959e1d245fa729530e61d53a64b46
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-03-23 21:34:14 -0300

    test: finalizando testes no store de autenticação

--- packages/apps/hub-web/src/__tests__/store/modules/auth/sagas.spec.ts
+++ packages/apps/hub-web/src/__tests__/store/modules/auth/sagas.spec.ts
@@ -2,9 +2,15 @@ import { Payload } from 'redux-saga/effects'
 import { runSaga } from 'redux-saga'
 
 import * as profileActions from '~/store/modules/profile/actions'
+import { productRequest } from '~/store/modules/products/actions'
 import * as globalActions from '~/store/modules/global/actions'
-import { AccessData, SignInRequest } from '~/store/modules/auth/types'
-import { signIn, prepareAccess } from '~/store/modules/auth/sagas'
+import * as types from '~/store/modules/auth/types'
+import {
+  signIn,
+  prepareAccess,
+  checkingExpiringToken,
+  refreshToken
+} from '~/store/modules/auth/sagas'
 import * as authActions from '~/store/modules/auth/actions'
 
 import { toast } from '@hub/common/utils'
@@ -60,7 +66,7 @@ describe('Sagas of authentication history', () => {
       const mockedAction = authActions.signInRequest({
         ...userMockSignIn,
         redirect: from
-      }) as Payload<SignInRequest>
+      }) as Payload<types.SignInRequest>
 
       jest
         .spyOn(eem, 'EEMConnectPost')
@@ -140,7 +146,7 @@ describe('Sagas of authentication history', () => {
         selected_profile: prepareAccessProfileMock.selected_profile,
         selected_school: prepareAccessProfileMock.selected_school,
         redirect: data.redirect
-      }) as Payload<AccessData>
+      }) as Payload<types.AccessData>
 
       jest
         .spyOn(eemIntegration, 'changeSchool')
@@ -148,6 +154,7 @@ describe('Sagas of authentication history', () => {
 
       mockState.user = {
         ...mockState.user,
+        school: userMock.school,
         user: {
           ...userMock.user,
           guid: data.guid || userMock.user.guid
@@ -205,6 +212,181 @@ describe('Sagas of authentication history', () => {
 
       expect(dispatchedActions).toContainObject(authActions.setSigned())
       expect(historyMock).toBeCalledWith('/solucao/teste-mock')
+    })
+  })
+
+  describe('checking Expiring Token', () => {
+    it('You should pause running if payload comes empty', async () => {
+      const mockedAction = {
+        type: 'persist/REHYDRATE',
+        payload: undefined as unknown
+      } as Payload<types.RehydrateAuth>
+
+      const resolved = await runSaga(
+        store,
+        checkingExpiringToken,
+        mockedAction
+      ).result()
+
+      expect(resolved).toBeUndefined()
+    })
+
+    it('Should not log the user if it does not have the active login flag', async () => {
+      const mockedAction = {
+        type: 'persist/REHYDRATE',
+        payload: {
+          auth: { signed: false }
+        }
+      } as Payload<types.RehydrateAuth>
+
+      await runSaga(store, checkingExpiringToken, mockedAction).toPromise()
+
+      expect(dispatchedActions).toContainObject(authActions.signOut())
+    })
+
+    it('Should return if the expiration date is not defined', async () => {
+      const mockedAction = {
+        type: 'persist/REHYDRATE',
+        payload: {
+          auth: {
+            exp: 0,
+            iat: 10,
+            token: 'fake-token',
+            signed: true,
+            reduced_token: 'fake-reduced-token'
+          },
+          user: {
+            user: {
+              guid: 'fake-id'
+            }
+          }
+        }
+      } as Payload<types.RehydrateAuth>
+
+      const resolved = await runSaga(
+        store,
+        checkingExpiringToken,
+        mockedAction
+      ).toPromise()
+
+      expect(resolved).toBeUndefined()
+    })
+
+    it('Should be set, in the reload of the application, the token in the Header of the API if the user is logged in', async () => {
+      const mockedAction = {
+        type: 'persist/REHYDRATE',
+        payload: {
+          auth: {
+            exp: new Date().getTime() + 10,
+            iat: 10,
+            token: 'fake-token',
+            signed: true,
+            reduced_token: 'fake-reduced-token'
+          },
+          user: {
+            user: {
+              guid: 'fake-id'
+            }
+          }
+        }
+      } as Payload<types.RehydrateAuth>
+
+      await runSaga(store, checkingExpiringToken, mockedAction).toPromise()
+
+      expect(dispatchedActions).toContainObject(
+        authActions.reducedTokenEEM('fake-reduced-token')
+      )
+
+      expect(dispatchedActions).toContainObject(
+        globalActions.enableRefreshTokenMiddleware(true)
+      )
+
+      expect(dispatchedActions).toContainObject(productRequest({}))
+    })
+
+    it('should refresh the user if the Token expiration date is less than the current date', async () => {
+      const mockedAction = {
+        type: 'persist/REHYDRATE',
+        payload: {
+          auth: {
+            exp: 10,
+            iat: 10,
+            token: 'fake-token',
+            signed: true,
+            reduced_token: 'fake-reduced-token'
+          },
+          user: {
+            user: {
+              guid: 'fake-id'
+            }
+          }
+        }
+      } as Payload<types.RehydrateAuth>
+
+      await runSaga(store, checkingExpiringToken, mockedAction).toPromise()
+
+      expect(dispatchedActions).toContainObject(
+        authActions.refreshTokenRequest()
+      )
+    })
+  })
+
+  describe('refresh Token', () => {
+    it('Should generate a new user authentication token for the user', async () => {
+      mockState.auth = {
+        ...mockState.auth,
+        refresh_token: authMock.refresh_token
+      }
+
+      const returnedMock = {
+        ok: true,
+        originalError: null,
+        problem: null,
+        data: fakeResponse
+      }
+
+      jest
+        .spyOn(eem, 'EEMConnectPost')
+        .mockImplementationOnce(() => Promise.resolve<any>(returnedMock))
+
+      jest
+        .spyOn(eemIntegration, 'changeSchool')
+        .mockImplementationOnce(() => Promise.resolve<any>(returnedMock))
+
+      await runSaga(store, refreshToken).toPromise()
+
+      expect(dispatchedActions).toContainObject({
+        payload: true,
+        type: '@global/ENABLE_REFRESH_TOKEN'
+      })
+
+      expect(dispatchedActions).toContainObject(productRequest({}))
+    })
+
+    it('You should discharge the user if something fails in the return of API', async () => {
+      mockState.auth = {
+        ...mockState.auth,
+        refresh_token: authMock.refresh_token
+      }
+
+      const returnedMock = {
+        ok: false,
+        originalError: null,
+        problem: null,
+        data: fakeResponse
+      }
+
+      jest
+        .spyOn(eem, 'EEMConnectPost')
+        .mockImplementationOnce(() => Promise.resolve<any>(returnedMock))
+
+      const mockHistory = jest.spyOn(history, 'push')
+
+      await runSaga(store, refreshToken).toPromise()
+
+      expect(dispatchedActions).toContainObject(authActions.signOut())
+
+      expect(mockHistory).toBeCalledWith('/login')
     })
   })
 })
