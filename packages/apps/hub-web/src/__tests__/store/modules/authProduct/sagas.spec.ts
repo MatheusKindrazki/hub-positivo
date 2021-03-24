b9364520fc4691204eb60f3084f756d0be4230c0
gabryelryba <gabryel.ryba@gmail.com>
2021-03-24 17:56:39 -0300

    test(store/modules/authproduct): testing sagas from authProduct

--- packages/apps/hub-web/src/__tests__/store/modules/authProduct/sagas.spec.ts
+++ packages/apps/hub-web/src/__tests__/store/modules/authProduct/sagas.spec.ts
@@ -1,23 +1,33 @@
-import { takeLatest, call, put } from 'redux-saga/effects'
+import { cloneDeep } from 'lodash'
+
+import { runSaga } from 'redux-saga'
 
 import { setFrameURL } from '~/store/modules/products/actions'
+import { loading } from '~/store/modules/global/actions'
 import {
   productSorting,
-  authProductEEM,
-  authProductGUID
+  authProductGUID,
+  authProductEEM
 } from '~/store/modules/authProduct/sagas'
 import {
-  Actions,
-  preAuth,
+  authProductFailure,
   authProductRequest,
-  authProductSuccess,
-  authProductFailure
+  authProductSuccess
 } from '~/store/modules/authProduct/actions'
-import { store } from '~/store'
+
+import { toast } from '@hub/common/utils'
+import { apiAuthProduct } from '@hub/api'
 
 import history from '~/services/history'
 
-import refreshToken from '~/middlewares/refreshToken'
+import isMobile from '~/utils/isMobile'
+
+import refreshTokenMiddleware from '~/middlewares/refreshToken'
+import store, { mockState } from '~/__mocks__/fakeStore.mock'
+
+jest.mock('~/middlewares/refreshToken')
+
+const pushSpy = jest.spyOn(history, 'push')
 
 const mockedPayload = {
   payload: {
@@ -32,99 +42,251 @@ const mockedPayload = {
 }
 
 describe('Testing productSorting saga flow', () => {
-  it('productSorting should call refresh token then dispatch a request action', () => {
-    const generator = productSorting(mockedPayload)
+  const clonedMockState = cloneDeep(mockState)
 
-    expect(generator.next().value.toString()).toStrictEqual(
-      call(refreshToken).toString()
-    )
-    expect(generator.next().value).toStrictEqual(
-      put(
-        authProductRequest(mockedPayload.payload, 'AUTH_PRODUCT_GUID_REQUEST')
-      )
-    )
-    expect(generator.next().done).toBeTruthy()
+  let dispatchedActions = store.getActions()
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    jest.clearAllTimers()
+    store.clearActions()
+
+    dispatchedActions = store.getActions()
   })
 
-  it.skip('productSorting should early return when no auth, profile or user info is available', () => {
-    const mockedStateNoInfo = {
-      auth: undefined,
-      profile: undefined,
-      user: undefined
-    }
-
-    jest
-      .spyOn(store, 'getState')
-      .mockImplementation(() => mockedStateNoInfo as any)
-
-    const generator = productSorting(mockedPayload)
-
-    expect(generator.next().done).toBeTruthy()
+  afterEach(() => {
+    mockState.auth = clonedMockState.auth
+    mockState.user = clonedMockState.user
+    mockState.profile = clonedMockState.profile
   })
 
-  it('productSorting should redirect user when tipoRenderizacao is `iframenoauth`', () => {
+  it('productSorting should call refresh token then dispatch a request action', async () => {
+    const refreshTokenMock = refreshTokenMiddleware as jest.Mock
+
+    await runSaga(store, productSorting, mockedPayload).toPromise()
+
+    expect(refreshTokenMock).toHaveBeenCalled()
+
+    expect(dispatchedActions).toContainObject(
+      authProductRequest(mockedPayload.payload, 'AUTH_PRODUCT_GUID_REQUEST')
+    )
+  })
+
+  it('productSorting should redirect user when tipoRenderizacao is `iframenoauth`', async () => {
     mockedPayload.payload.tipoRenderizacao = 'iframenoauth'
 
-    const pushSpy = jest.spyOn(history, 'push')
+    const refreshTokenMock = refreshTokenMiddleware as jest.Mock
 
-    const generator = productSorting(mockedPayload)
+    await runSaga(store, productSorting, mockedPayload).toPromise()
 
-    expect(generator.next().value.toString()).toStrictEqual(
-      call(refreshToken).toString()
+    expect(refreshTokenMock).toHaveBeenCalled()
+
+    expect(dispatchedActions).toContainObject(
+      setFrameURL({
+        url: 'http://produtoteste.com',
+        name: 'Produto Teste'
+      })
     )
 
-    expect(generator.next().value).toStrictEqual(
-      put(
-        setFrameURL({
-          url: mockedPayload.payload.url,
-          name: mockedPayload.payload.name
-        })
-      )
-    )
-
-    expect(pushSpy).toHaveBeenCalledWith(
-      `/solucao/${mockedPayload.payload.product}`
-    )
-
-    expect(generator.next().done).toBeTruthy()
+    expect(pushSpy).toHaveBeenCalledWith('/solucao/Teste')
   })
 
-  it('productSorting should open a new page when tipoRenderizacao is `targetblank`', () => {
+  it('productSorting should open a new page when tipoRenderizacao is `targetblank`', async () => {
     mockedPayload.payload.tipoRenderizacao = 'targetblank'
 
-    global.open = jest.fn() as jest.Mock
+    const spyWindowOpen = jest
+      .spyOn(global, 'open')
+      .mockImplementation(jest.fn())
 
-    const generator = productSorting(mockedPayload)
+    const refreshTokenMock = refreshTokenMiddleware as jest.Mock
 
-    expect(generator.next().value.toString()).toStrictEqual(
-      call(refreshToken).toString()
-    )
+    await runSaga(store, productSorting, mockedPayload).toPromise()
 
-    expect(generator.next().value).toStrictEqual(put(authProductSuccess()))
+    expect(refreshTokenMock).toHaveBeenCalled()
 
-    expect(global.open).toHaveBeenCalledWith(
-      mockedPayload.payload.url,
+    expect(dispatchedActions).toContainObject(authProductSuccess())
+
+    expect(spyWindowOpen).toHaveBeenCalledWith(
+      'http://produtoteste.com',
       '_blank'
     )
-
-    expect(generator.next().done).toBeTruthy()
   })
 
-  it('productSorting should dispatch request action when tipoRenderizacao is `iframe`', () => {
+  it('productSorting should dispatch request action when tipoRenderizacao is `iframe`', async () => {
     mockedPayload.payload.tipoRenderizacao = 'iframe'
 
     global.open = jest.fn() as jest.Mock
+    const refreshTokenMock = refreshTokenMiddleware as jest.Mock
 
-    const generator = productSorting(mockedPayload)
+    await runSaga(store, productSorting, mockedPayload).toPromise()
 
-    expect(generator.next().value.toString()).toStrictEqual(
-      call(refreshToken).toString()
+    expect(refreshTokenMock).toHaveBeenCalled()
+
+    expect(dispatchedActions).toContainObject(
+      authProductRequest(mockedPayload.payload, 'AUTH_PRODUCT_EEM_REQUEST')
     )
+  })
 
-    expect(generator.next().value).toStrictEqual(
-      put(authProductRequest(mockedPayload.payload, 'AUTH_PRODUCT_EEM_REQUEST'))
+  it('productSorting should early return when no auth, profile or user info is available', async () => {
+    mockState.auth = (undefined as unknown) as any
+    mockState.profile = (undefined as unknown) as any
+    mockState.user = (undefined as unknown) as any
+
+    await runSaga(store, productSorting, mockedPayload).toPromise()
+
+    expect(dispatchedActions.length).toBe(0)
+  })
+})
+
+describe('testing authProductGUID saga flow', () => {
+  const clonedMockState = cloneDeep(mockState)
+
+  let dispatchedActions = store.getActions()
+
+  const mockedResponse = {
+    data: 'test',
+    ok: true
+  }
+
+  afterEach(() => {
+    mockState.auth = clonedMockState.auth
+    mockState.user = clonedMockState.user
+    mockState.profile = clonedMockState.profile
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    jest.clearAllTimers()
+    store.clearActions()
+
+    dispatchedActions = store.getActions()
+  })
+
+  it('should set loading, make a post request, redirect user, set iframe and dispatch success action', async () => {
+    const { payload } = mockedPayload
+    const apiPostArgs = {
+      product: payload.product,
+      token: mockState.auth.token,
+      reduced_token: mockState.auth.reduced_token,
+      logged_in: {
+        school: {
+          name: mockState.user.school?.label,
+          id: mockState.user.school?.value,
+          class: mockState.educationalStage.level
+        },
+        profile: mockState.profile.guid,
+        user_id: mockState.user.user?.integration_id
+      },
+      expire_in: mockState.auth.exp
+    }
+    const headers = { headers: { Authorization: 'Bearer null' } }
+
+    const spyPost = jest
+      .spyOn(apiAuthProduct, 'post')
+      .mockImplementation(() => Promise.resolve<any>(mockedResponse))
+
+    await runSaga(store, authProductGUID, mockedPayload).toPromise()
+
+    expect(dispatchedActions).toContainObject(loading(true))
+    expect(spyPost).toHaveBeenCalledWith(
+      'api/TokenStorage',
+      apiPostArgs,
+      headers
     )
+    expect(pushSpy).toHaveBeenCalledWith('/solucao/Teste/')
+    expect(dispatchedActions).toContainObject(
+      setFrameURL({
+        url: 'http://produtoteste.com/test/',
+        name: 'Produto Teste'
+      })
+    )
+    expect(dispatchedActions).toContainObject(loading(false))
+    expect(dispatchedActions).toContainObject(authProductSuccess())
+  })
 
-    expect(generator.next().done).toBeTruthy()
+  it('should redirect user to correct path when payload has a subpath', async () => {
+    const mockedPayloadWithSubpath = {
+      payload: {
+        tipoRenderizacao: '',
+        url: 'http://produtoteste.com',
+        product: 'Teste',
+        name: 'Produto Teste',
+        subpath: 'subpath'
+      },
+      error: undefined,
+      meta: undefined,
+      type: ''
+    }
+    await runSaga(store, authProductGUID, mockedPayloadWithSubpath).toPromise()
+
+    expect(pushSpy).toHaveBeenCalledWith('/solucao/Teste/subpath')
+  })
+
+  it('should send a tost and dispatch failure action when post response has a error', async () => {
+    mockedResponse.ok = false
+    const spyToast = jest.spyOn(toast, 'error')
+    const spyPost = jest
+      .spyOn(apiAuthProduct, 'post')
+      .mockImplementation(() => Promise.resolve<any>(mockedResponse))
+
+    await runSaga(store, authProductGUID, mockedPayload).toPromise()
+
+    expect(dispatchedActions).toContainObject(loading(true))
+    expect(spyPost).toHaveBeenCalled()
+    expect(spyToast).toHaveBeenCalledWith('Sinto muito, algo deu errado :(')
+    expect(dispatchedActions).toContainObject(loading(false))
+    expect(dispatchedActions).toContainObject(authProductFailure())
+  })
+
+  it('productSorting should early return when no auth, profile or user info is available', async () => {
+    mockState.auth = (undefined as unknown) as any
+    mockState.profile = (undefined as unknown) as any
+    mockState.user = (undefined as unknown) as any
+
+    await runSaga(store, authProductGUID, mockedPayload).toPromise()
+
+    expect(dispatchedActions.length).toBe(0)
+  })
+})
+
+describe('testing authProductEEM saga flow', () => {
+  let dispatchedActions = store.getActions()
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    jest.clearAllTimers()
+    store.clearActions()
+
+    dispatchedActions = store.getActions()
+  })
+
+  it('should set loading, set iframe url and then dispatch success action', async () => {
+    await runSaga(store, authProductEEM, mockedPayload).toPromise()
+    expect(dispatchedActions).toContainObject(loading(true))
+    expect(dispatchedActions).toContainObject(
+      setFrameURL({
+        url: 'http://produtoteste.com',
+        name: 'Produto Teste'
+      })
+    )
+    expect(dispatchedActions).toContainObject(loading(true))
+    expect(dispatchedActions).toContainObject(authProductSuccess())
+  })
+
+  it('should open a new window when tipoRenderizacao is `iframeblank` and the device is IOS', async () => {
+    jest.spyOn(isMobile, 'iOS').mockReturnValue(true)
+    mockedPayload.payload.tipoRenderizacao = 'iframeblank'
+    const spyWindowOpen = jest
+      .spyOn(global, 'open')
+      .mockImplementation(jest.fn())
+
+    await runSaga(store, authProductEEM, mockedPayload).toPromise()
+    expect(dispatchedActions).toContainObject(loading(true))
+    expect(spyWindowOpen).toHaveBeenCalledWith(
+      'http://produtoteste.com',
+      '_blank'
+    )
+    expect(dispatchedActions).toContainObject(loading(true))
+    expect(dispatchedActions).toContainObject(authProductSuccess())
   })
 })
