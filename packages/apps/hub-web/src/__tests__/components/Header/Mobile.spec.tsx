5fd7badd40f5b785f1a1c621ce3dac26540b6d54
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-24 12:31:09 -0300

    feat: testes automatizados
    testando se os elementos do drawer mudam de cor quando inseridos na pagina de minhas turmas

--- packages/apps/hub-web/src/__tests__/components/Header/Mobile.spec.tsx
+++ packages/apps/hub-web/src/__tests__/components/Header/Mobile.spec.tsx
@@ -1,11 +1,13 @@
 import React, { useRef } from 'react'
 
 import reactEvent from 'react-select-event'
+import * as reactRouter from 'react-router'
 import { renderHook } from '@testing-library/react-hooks'
 
 import { store } from '~/store'
 
-import { render, CustomState, fireEvent, waitFor, act } from '@hub/test-utils'
+import { render, CustomState, fireEvent, act, Providers } from '@hub/test-utils'
+import { useTheme } from '@hub/common/layout/styles'
 import * as drawer from '@hub/common/components/Drawer'
 
 import { UseDisclosureProps } from '@chakra-ui/react'
@@ -37,7 +39,9 @@ jest.mock('react-router', () => {
   return {
     ...rest,
     useHistory: jest.fn(() => ({
-      location: '/'
+      location: {
+        pathname: '/'
+      }
     }))
   }
 })
@@ -62,11 +66,26 @@ describe('Mobile Header component', () => {
     })
   }
 
+  const {
+    result: {
+      current: { colors }
+    }
+  } = renderHook(() => useTheme(), { wrapper: Providers })
+
   const useDisclosureFunctions = {
     onClose: jest.fn(),
     onOpen: jest.fn(),
     isOpen: true
   }
+
+  const {
+    defaultValue,
+    resetInfo,
+    schoolList,
+    roleList,
+    setSchool,
+    setRole
+  } = useHeaderReturn
 
   beforeEach(() => {
     spyUseDisclosure(useDisclosureFunctions)
@@ -97,13 +116,16 @@ describe('Mobile Header component', () => {
       }
     )
 
-    const blurEffect = (element: HTMLElement): boolean =>
-      fireEvent.keyDown(element, { key: 'Esc', code: 27 })
-
     const [menuButton] = wrapper.getAllByRole('button')
-    wrapper.storeUtils?.clearActions()
-    return { ...wrapper, blurEffect, handleMenuClick, ref, menuButton }
+    return { ...wrapper, handleMenuClick, menuButton }
   }
+
+  it('Should have a button called `Estou com uma dúvida`', () => {
+    const { getByText } = setup()
+    const helpButton = getByText(/Estou com uma dúvida/i)
+
+    expect(helpButton).toBeInTheDocument()
+  })
 
   it('openMenu should call onOpen when isOpen is false', () => {
     const onOpen = jest.fn()
@@ -115,6 +137,7 @@ describe('Mobile Header component', () => {
 
     expect(handleMenuClick).toHaveBeenCalledTimes(1)
     expect(onOpen).toHaveBeenCalledTimes(1)
+    expect(resetInfo).toHaveBeenCalledTimes(1)
   })
 
   it('openMenu should call onClose when isOpen is true', () => {
@@ -161,12 +184,98 @@ describe('Mobile Header component', () => {
     ])
   })
 
-  it('Should call  useDisclosure`s onClose function when key of escape is pressed (ESC)', () => {
+  it('Should redirect to `/minhas-turmas`when `Minhas turmas` button is clicked', () => {
+    const { getByText } = setup({
+      profile: { guid: 'PROFESSOR' }
+    })
+    const classesButton = getByText(/Minhas turmas/i)
+    const homeButton = getByText(/Home/i)
+
+    expect(classesButton).toBeInTheDocument()
+
+    console.log(colors.blue[500])
+
+    // expect(homeButton).toHaveStyle(`color: ${colors.blue[500]}`)
+    // expect(classesButton).toHaveStyle(`color: ${colors.gray[500]}`)
+
+    fireEvent.click(classesButton)
+    expect(spyPush).toHaveBeenCalledWith('/minhas-turmas')
+  })
+
+  it('Should call useDisclosure`s onClose function when ´Home` button is clicked', () => {
+    const { getByText } = setup()
+
+    const homeButton = getByText(/Home/i)
+
+    fireEvent.click(homeButton)
+    expect(useDisclosureFunctions.onClose).toHaveBeenCalledTimes(1)
+    expect(spyPush).toHaveBeenCalledWith('/')
+  })
+
+  it('Should call useDisclosure`s onClose function when key of escape is pressed (ESC)', () => {
     const { getByTestId } = setup()
     const drawerContent = getByTestId('hub-drawer-content')
 
     fireEvent.keyDown(drawerContent, { key: 'Esc', code: 27 })
 
     expect(useDisclosureFunctions.onClose).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should change the `school` on selector when other `school` is triggered', async () => {
+    const { getByText, findByText } = setup()
+
+    const schoolLabel = defaultValue.school?.label as string
+    const school = await findByText(schoolLabel)
+
+    fireEvent.mouseDown(school)
+
+    expect(school).toBeInTheDocument()
+    reactEvent.openMenu(school)
+    const otherSchool = getByText(schoolList[0].label)
+    expect(otherSchool).toBeInTheDocument()
+
+    fireEvent.click(otherSchool)
+
+    expect(setSchool).toHaveBeenLastCalledWith({
+      label: schoolList[0].label,
+      roles: schoolList[0].roles,
+      value: schoolList[0].value
+    })
+  })
+
+  it('Should change the `role` on selector when other `role` is triggered', async () => {
+    const { getByText, findByText } = setup()
+    const roleLabel = defaultValue.role?.label as string
+    const selectedRole = await findByText(roleLabel)
+    fireEvent.mouseDown(selectedRole)
+
+    expect(selectedRole).toBeInTheDocument()
+    reactEvent.openMenu(selectedRole)
+
+    const coordenador = roleList[1]
+
+    const otherRole = getByText(coordenador.name)
+    expect(otherRole).toBeInTheDocument()
+
+    fireEvent.click(otherRole)
+
+    expect(setRole).toHaveBeenLastCalledWith(coordenador)
+  })
+
+  it('Should change `Home` and `Minhas turmas` buttons styles when location is /minhas-turmas', async () => {
+    jest.spyOn(reactRouter, 'useHistory').mockReturnValue({
+      location: {
+        pathname: '/minhas-turmas'
+      }
+    } as any)
+    const { getByText } = setup({
+      profile: { guid: 'PROFESSOR' }
+    })
+
+    const homeButton = getByText(/Home/i)
+    const classesButton = getByText(/Minhas turmas/i)
+
+    // expect(homeButton).toHaveStyle(`color: ${colors.gray[500]}`)
+    // expect(classesButton).toHaveStyle(`color: ${colors.blue[500]}`)
   })
 })
