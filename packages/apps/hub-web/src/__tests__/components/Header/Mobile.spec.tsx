3dd01b18078c81fbb4dbf35d572b0b2367be25ab
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-24 10:46:44 -0300

    feat(mobile.spec.tsx): testes automatizados
    testando se onOpen e onClose do Header mobile funcinam corretamente

--- packages/apps/hub-web/src/__tests__/components/Header/Mobile.spec.tsx
+++ packages/apps/hub-web/src/__tests__/components/Header/Mobile.spec.tsx
@@ -5,7 +5,10 @@ import { renderHook } from '@testing-library/react-hooks'
 
 import { store } from '~/store'
 
-import { render, CustomState, fireEvent, waitFor } from '@hub/test-utils'
+import { render, CustomState, fireEvent, waitFor, act } from '@hub/test-utils'
+import * as drawer from '@hub/common/components/Drawer'
+
+import { UseDisclosureProps } from '@chakra-ui/react'
 
 import history from '~/services/history'
 
@@ -49,15 +52,32 @@ describe('Mobile Header component', () => {
 
   const spyPush = jest.spyOn(history, 'push')
 
+  const spyUseDisclosure = (alterations: Partial<UseDisclosureProps>): void => {
+    const {
+      result: { current }
+    } = renderHook(() => drawer.useDisclosure())
+    jest.spyOn(drawer, 'useDisclosure').mockReturnValue({
+      ...current,
+      ...alterations
+    })
+  }
+
   const setup = (CUSTOM_STATE = { ...userState } as CustomState) => {
     const {
       result: { current: ref }
     } = renderHook(() => useRef<RefMenuProps>(null))
+
     const openModalPass = jest.fn()
-    // const handleClick
+
+    const handleMenuClick = jest.fn(() => {
+      act(() => {
+        ref.current?.openMenu()
+      })
+    })
+
     const wrapper = render(
       <>
-        {/* <MenuButton onClick={handleClick} /> */}
+        <MenuButton onClick={handleMenuClick} />
         <MobileMenu openModalPass={openModalPass} ref={ref} />
       </>,
       {
@@ -66,16 +86,38 @@ describe('Mobile Header component', () => {
         CUSTOM_STATE
       }
     )
-    ref.current?.openMenu()
 
     const blurEffect = (element: HTMLElement): boolean =>
       fireEvent.keyDown(element, { key: 'Esc', code: 27 })
 
     wrapper.storeUtils?.clearActions()
-    return { ...wrapper, blurEffect, ref }
+    return { ...wrapper, blurEffect, handleMenuClick, ref }
   }
-  it('it', () => {
-    const { debug } = setup()
-    debug()
+
+  it('openMenu should call onOpen when isOpen is false', () => {
+    const onOpen = jest.fn()
+    spyUseDisclosure({ onOpen, isOpen: false })
+
+    const { handleMenuClick, getByRole } = setup()
+    const menuButton = getByRole('button')
+
+    fireEvent.click(menuButton)
+
+    expect(handleMenuClick).toHaveBeenCalledTimes(1)
+    expect(onOpen).toHaveBeenCalledTimes(1)
+  })
+
+  it('openMenu should call onClose when isOpen is true', () => {
+    const onClose = jest.fn()
+
+    spyUseDisclosure({ onClose, isOpen: true })
+
+    const { handleMenuClick, getAllByRole } = setup()
+    const [menuButton] = getAllByRole('button')
+
+    fireEvent.click(menuButton)
+
+    expect(handleMenuClick).toHaveBeenCalledTimes(1)
+    expect(onClose).toHaveBeenCalledTimes(1)
   })
 })
