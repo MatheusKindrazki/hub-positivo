57f2f13c004bb879a39f381b715d9ca0aeea7360
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-03-24 17:12:58 -0300

    feat: testes automatizados
    testando header do hub

--- packages/apps/hub-web/src/__tests__/components/Header/Header.spec.tsx
+++ packages/apps/hub-web/src/__tests__/components/Header/Header.spec.tsx
@@ -1,9 +1,58 @@
 import React from 'react'
 
+import MatchMediaMock from 'jest-matchmedia-mock'
+import { renderHook } from '@testing-library/react-hooks'
+
+import { store } from '~/store'
+
 import { render } from '@hub/test-utils'
+import * as drawer from '@hub/common/components/Drawer'
+
+import Header from '~/components/Header'
+
+jest.mock('react-router', () => {
+  const rest = jest.requireActual('react-router')
+  return {
+    ...rest,
+    useHistory: jest.fn(() => ({
+      location: {
+        pathname: '/'
+      }
+    }))
+  }
+})
 
 describe('get started', () => {
-  it('it', () => {
-    expect(1).toBe(1)
+  jest.spyOn(drawer, 'useDisclosure').mockReturnValue({
+    isOpen: true
+  } as any)
+
+  const setup = () => {
+    const wrapper = render(<Header />, {
+      store,
+      reducers: ['tour', 'user', 'profile']
+    })
+
+    return { ...wrapper }
+  }
+  it('Should render Desktop Header when min-width is 480px', () => {
+    const matchmedia = new MatchMediaMock()
+    matchmedia.useMediaQuery('(min-width: 480px)')
+
+    const { getByTestId } = setup()
+    const popOverContent = getByTestId('hub-popover-content')
+
+    expect(popOverContent).toBeInTheDocument()
+  })
+
+  it('Should render Mobile Header when min-width is 479px', () => {
+    const matchmedia = new MatchMediaMock()
+    matchmedia.useMediaQuery('(min-width: 479px)')
+
+    const { getByTestId } = setup()
+
+    const drawerContent = getByTestId('hub-drawer-content')
+
+    expect(drawerContent).toBeInTheDocument()
   })
 })
--- packages/apps/hub-web/src/__tests__/components/Header/context.spec.tsx
+++ packages/apps/hub-web/src/__tests__/components/Header/context.spec.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import { renderHook } from '@testing-library/react-hooks'
+
+import { store } from '~/store'
+
 import { render } from '@hub/test-utils'
 
-describe('get started', () => {
+import { useHeader, HeaderProvider } from '~/components/Header/context'
+import Header from '~/components/Header'
+
+describe('Testing useHeader', () => {
   it('it', () => {
-    expect(1).toBe(1)
+    // const {
+    //   result: { current }
+    // } = renderHook(() => useHeader(), { wrapper: Header })
+    // console.log(current)
   })
 })
