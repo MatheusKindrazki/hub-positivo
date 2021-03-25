4085bc976177b7c1913b3cf8e2b613189f517f08
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-03-25 00:06:14 -0300

    fix: identificando usuário

--- packages/apps/hub-web/src/services/mixpanel/setProperties.ts
+++ packages/apps/hub-web/src/services/mixpanel/setProperties.ts
@@ -16,32 +16,32 @@ import {
 const { user, school } = store.getState().user
 
 const schools_list = user?.schools?.map(s => s.name)
-
-const sendProps = {
-  ...activeProfiles,
-  ...activeStages,
-  selected_class,
-  selected_educational_stage,
-
-  user_id: user?.guid,
-  user_login: user?.username,
-  user_name: user?.name,
-  user_mail: user?.email,
-
-  selected_role: profile.name,
-  roles_list: profileNames,
-  selected_school_id: school?.value,
-  selected_school_name: school?.label,
-  schools_list
-}
-
 const setUserProperties = (): void => {
+  const sendProps = {
+    ...activeProfiles,
+    ...activeStages,
+    selected_class,
+    selected_educational_stage,
+
+    user_id: user?.guid,
+    user_login: user?.username,
+    user_name: user?.name,
+    user_mail: user?.email,
+
+    selected_role: profile.name,
+    roles_list: profileNames,
+    selected_school_id: school?.value,
+    selected_school_name: school?.label,
+    schools_list
+  }
+
   const { signed } = store.getState().auth
 
   if (!signed) return
-
   try {
-    mixpanel?.people?.set(sendProps)
+    mixpanel.people.set(sendProps)
+
+    mixpanel.identify(user?.guid)
   } catch (error) {
     console.log('Erro ao identificar usuário via mixpanel')
   }
