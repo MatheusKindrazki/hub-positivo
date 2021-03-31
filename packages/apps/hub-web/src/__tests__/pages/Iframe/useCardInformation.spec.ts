f5fe6ddc87f6342bac554dc68738ee6a3cdbeb59
gabryelryba <gabryel.ryba@gmail.com>
2021-03-31 09:00:54 -0300

    test(pages/iframe/hook): useCardInformation tests

--- packages/apps/hub-web/src/__tests__/pages/Iframe/useCardInformation.spec.ts
+++ packages/apps/hub-web/src/__tests__/pages/Iframe/useCardInformation.spec.ts
@@ -41,30 +41,36 @@ jest.mock('react-router-dom', () => {
 })
 
 describe('useCardInformation should work as expected', () => {
+  afterEach(jest.clearAllMocks)
+
   const spyPush = jest.spyOn(history, 'push').mockImplementation()
   const spyDispatch = jest.spyOn(store, 'dispatch')
   const dispatchedAction = {
     payload: {
       name: 'test-card',
       product: 'testcard',
-      subpath: '',
+      subpath: 'subpath',
       tipoRenderizacao: 'wordpress',
       url: 'http://test-card.com'
     },
     type: '@auth/AUTH_PRODUCT_REQUEST'
   }
 
-  it('useCardInformation creates card and dispatches preAuth action', async () => {
+  it('useCardInformation gets card slug and dispatches preAuth action', async () => {
+    jest.spyOn(router, 'useParams').mockReturnValue({
+      solution: 'solution',
+      subpath: 'subpath'
+    })
     const spyGetCardBySlug = jest
       .spyOn(service, 'getCardBySlug')
-      .mockImplementationOnce(() => Promise.resolve<any>(cardSlug))
+      .mockImplementation(() => Promise.resolve<any>(cardSlug))
 
     renderHook(() => useCardInformation())
 
     expect(spyGetCardBySlug).toHaveBeenCalledWith({
       nivelEnsino: 'fake-level',
       perfil: 'th1s-1s-4-f4k3-gu1d',
-      slug: 'fake-solution'
+      slug: 'solution'
     })
     await waitFor(() =>
       expect(spyDispatch).toHaveBeenCalledWith(dispatchedAction)
@@ -72,34 +78,44 @@ describe('useCardInformation should work as expected', () => {
   })
 
   it('if getCardBySlug returns no card user should be redirected', async () => {
+    jest.spyOn(router, 'useParams').mockReturnValue({
+      solution: 'solution',
+      subpath: 'subpath'
+    })
     jest
       .spyOn(service, 'getCardBySlug')
-      .mockImplementation(() => Promise.resolve<any>(undefined))
+      .mockImplementationOnce(() => Promise.resolve<any>(undefined))
 
     renderHook(() => useCardInformation())
     await waitFor(() => expect(spyPush).toHaveBeenCalledWith('/'))
   })
 
-  it('should dispatch preAuth action with subpath as an empty string when path includes undefined', async () => {
-    jest
-      .spyOn(service, 'getCardBySlug')
-      .mockImplementation(() => Promise.resolve<any>(cardSlug))
-
-    // mudando implementacao do useParams com spyOn
+  it('should dispatch preAuth action with subpath as an empty string when path is undefined', async () => {
+    dispatchedAction.payload.subpath = ''
     jest
       .spyOn(router, 'useParams')
-      .mockReturnValueOnce({ solution: 'asd', subpath: 'undefined' })
+      .mockReturnValueOnce({ solution: 'solution', subpath: undefined })
 
     renderHook(() => useCardInformation())
-    await waitFor(() => expect(spyPush).toHaveBeenCalledWith('/'))
+    await waitFor(() =>
+      expect(spyDispatch).toHaveBeenCalledWith(dispatchedAction)
+    )
   })
 
   it('should set url as empty string when given cardSlug has no link', async () => {
+    // alterando mock para que url no payload seja uma string vazia
+    dispatchedAction.payload.url = ''
+    dispatchedAction.payload.subpath = ''
+    jest
+      .spyOn(router, 'useParams')
+      .mockReturnValueOnce({ solution: 'solution', subpath: 'undefined' })
     jest
       .spyOn(service, 'getCardBySlug')
-      .mockImplementation(() => Promise.resolve<any>(cardSlugWithoutLink))
+      .mockImplementationOnce(() => Promise.resolve<any>(cardSlugWithoutLink))
 
     renderHook(() => useCardInformation())
-    await waitFor(() => expect(spyPush).toHaveBeenCalledWith('/'))
+    await waitFor(() =>
+      expect(spyDispatch).toHaveBeenCalledWith(dispatchedAction)
+    )
   })
 })
