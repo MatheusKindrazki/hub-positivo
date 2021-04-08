d4a040d500b4ac8e2b7cc9f70b90b88fba64700c
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-04-08 19:05:56 -0300

    fix: ignorando erros não tratados vindos do postMessage dos iframes

--- packages/apps/hub-web/src/hooks/usePostMessage.ts
+++ packages/apps/hub-web/src/hooks/usePostMessage.ts
@@ -30,6 +30,9 @@ const usePostMessage = (): void => {
 
   useEffect(() => {
     function handleEvent(event: { data: string }): void {
+      // Ignora erros vindos do postMessage
+      window.newrelic?.setErrorHandler(() => false)
+
       if (typeof event.data === 'string') {
         if (!hasJsonStructure(event.data)) return
 
