f11e665819e20a4313e3b13c633057eb1cbd973a
gabryelryba <gabryel.ryba@gmail.com>
2021-04-09 08:43:55 -0300

    refactor: refactorando codigos estranhos

--- packages/apps/hub-web/src/services/getSiteControl/gscOnSubmit.ts
+++ packages/apps/hub-web/src/services/getSiteControl/gscOnSubmit.ts
@@ -1,14 +1,15 @@
 import mixpanel from 'mixpanel-browser'
+
 // utilizar formatacao dos dados quando a funcao estiver pronta
-// import formatWidgetData from '~/utils/formatData/gscWidgetData'
+import formatWidgetData from '~/utils/formatData/gscWidgetData'
 
 const widgetSubmitEvent = 'Widget submitted'
 
-const setGSCOnSubmit = () => {
+const setGSCOnSubmit = (): void => {
   if (window.gsc) {
     window.gsc('onSubmit', (widgetId, data) => {
       try {
-        mixpanel.track(widgetSubmitEvent, { widgetId, ...data })
+        mixpanel.track(widgetSubmitEvent, formatWidgetData(widgetId, data))
       } catch (error) {
         console.error('Erro ao capturar submissao do widget do gsc no mixpanel')
       }
--- packages/apps/hub-web/src/utils/formatData/gscWidgetData.ts
+++ packages/apps/hub-web/src/utils/formatData/gscWidgetData.ts
@@ -1,42 +1,52 @@
 import { format } from 'date-fns'
 
 interface LooseObject {
-  [key: string]: boolean
+  [key: string]: boolean | string[] | string
 }
 
-interface WidgetData {
-  time: number | string
-  radio?: LooseObject
-  text?: string
-  textarea?: string
-  checkboxes?: LooseObject
-  select?: LooseObject
-  rating?: LooseObject
-  checkbox?: boolean
-}
-
-interface FormattedWidgetData extends WidgetData {
-  widgetId: number
-}
-
-// const formatWidgetButtons = (data: LooseObject): string[] | string => {
-//   const key = Object.keys(data)
-//   if ((key.length = 1)) return key[0]
-
-//   return key
+// interface WidgetData {
+//   time: number | string
+//   radio?: LooseObject
+//   text?: string
+//   textarea?: string
+//   checkboxes?: LooseObject
+//   select?: LooseObject
+//   rating?: LooseObject
+//   checkbox?: boolean
 // }
 
-const formatWidgetData = (widgetId: number, data: WidgetData) => {
+interface WidgetData {
+  [index: string]: any
+}
+
+interface FormattedWidgetData {
+  [index: string]: any
+}
+
+const formatWidgetButtons = (data: LooseObject): string[] | string => {
+  const key = Object.keys(data)
+  if (key.length === 1) return key[0]
+
+  return key
+}
+
+const formatWidgetData = (
+  widgetId: number,
+  data: WidgetData
+): FormattedWidgetData => {
   const formattedObject = {} as FormattedWidgetData
   formattedObject.widgetId = widgetId
 
   // Para que os campos de botoes nao sejam objetos com booleanos, podemos filtrar os botoes deixando-os com o nome da chave somente
-  // const inputKeys = Object.keys(data)
-  // inputKeys.map(key => {
-  //   if (key === 'radio' || 'checkboxes' || 'select' || 'rating') {
-  //     formattedObject[key as K] = formatWidgetButtons(data[key])
-  //   }
-  // })
+  const inputKeys = Object.keys(data)
+
+  console.log(inputKeys)
+  inputKeys.map(key => {
+    // eslint-disable-next-line no-constant-condition
+    if (key === 'radio' || 'checkboxes' || 'select' || 'rating') {
+      formattedObject[key] = formatWidgetButtons(data[key])
+    }
+  })
 
   if (typeof data.time === 'number') {
     formattedObject.time = format(data.time, 'dd MM yyyy, H:mm:ss')
