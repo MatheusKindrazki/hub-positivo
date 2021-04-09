d4eefebaaeab524c3bece5151d6e1c26ba022fef
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-04-09 17:32:16 -0300

    test: criar testes automatizados para os utilitarios do refresh token

--- packages/apps/hub-web/src/utils/enableRefreshToken.ts
+++ packages/apps/hub-web/src/utils/enableRefreshToken.ts
@@ -3,11 +3,11 @@ import { store } from '~/store'
 import roundHours from './formatData/roundHours'
 
 const enableRefreshToken = (exp: number): boolean => {
-  const expirationDateInMs = exp * 1000
   const { enableMiddlewareRefreshToken } = store.getState().global
   const date = new Date().getTime()
+
+  const expirationDateInMs = roundHours({ milliseconds: exp * 1000 }).getTime()
   const nowInMs = roundHours({ milliseconds: date }).getTime()
-  console.log(expirationDateInMs, nowInMs)
 
   if (nowInMs >= expirationDateInMs && enableMiddlewareRefreshToken) {
     return true
