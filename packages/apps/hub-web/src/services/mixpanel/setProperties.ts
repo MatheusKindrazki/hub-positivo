c61d25a61ecdc4822c907ccdb499d8d5ceaf8af7
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-04-14 16:29:08 -0300

    chore: implementação do gsc

--- packages/apps/hub-web/src/services/mixpanel/setProperties.ts
+++ packages/apps/hub-web/src/services/mixpanel/setProperties.ts
@@ -9,8 +9,11 @@ import { store } from '~/store'
 import profiles from '~/utils/formatData/profile'
 import educationalStage from '~/utils/formatData/educationalStage'
 
+import ctpmSchools from './mock/ctpmSchools.json'
+
 const setProperties = (): void => {
   const { info: user, school } = store.getState().user
+  const educational = store.getState().educationalStage
 
   const schools_list = user?.schools?.map(s => s.name)
 
@@ -18,15 +21,16 @@ const setProperties = (): void => {
 
   const {
     activeStages,
-    selected_class,
+
     selected_educational_stage
   } = educationalStage()
 
   const sendProps = {
     ...activeProfiles,
     ...activeStages,
-    selected_class,
+    selected_class: educational.class,
     selected_educational_stage,
+    is_ctpm: ctpmSchools.includes(school?.value || ''),
 
     user_id: user?.guid,
     user_login: user?.username,
@@ -57,6 +61,13 @@ const setProperties = (): void => {
     'user_name',
     sendProps.user_name as string
   )
+
+  // Identificador Get Site Control
+  if (window.gsc) {
+    window?.gsc('params', {
+      ...sendProps
+    })
+  }
 
   try {
     mixpanel.people.set(sendProps)
