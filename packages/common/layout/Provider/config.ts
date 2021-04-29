e197f24367b04bf037cb8e9b8088d9ef91885dca
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-04-29 09:50:24 -0300

    fix: ajustando hash de css class

--- packages/common/layout/Provider/config.ts
+++ packages/common/layout/Provider/config.ts
@@ -4,6 +4,4 @@ import getRandom from '../../utils/randomNumber'
 
 const generateKey = generate(getRandom(10, 15)).replace(/[0-9]/g, '')
 
-export const cssKey = !process.env.REACT_APP_IS_HUB
-  ? 'hub-component'
-  : `hub-${generateKey.toLowerCase()}`
+export const cssKey = `hub-${generateKey.toLowerCase()}`
