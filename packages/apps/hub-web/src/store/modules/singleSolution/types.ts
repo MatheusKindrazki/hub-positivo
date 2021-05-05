3f39a11eb3c1adbf5deaa2209d070d8274967694
gabryelryba <gabryel.ryba@gmail.com>
2021-05-05 18:07:21 -0300

    refactor(store): criado modulo na store para solucoes individuais

---
+++ packages/apps/hub-web/src/store/modules/singleSolution/types.ts
@@ -0,0 +1,37 @@
+export interface Solution {
+  id: string
+  nome: string
+  descricao: string
+  arquivo: string
+  link: string
+  idCategoria: string
+  ativo: boolean
+  tipoRenderizacao: string
+  slug: string
+  dataExclusao: string
+  ordem: number
+  excluida: boolean
+}
+
+export interface SolutionReducer {
+  loading: boolean
+  solution: Solution | null
+}
+
+export interface GetSolutionBySlugPayload {
+  slug: string
+  profile: string
+  educationalLevel: string
+}
+
+export interface PostSolutionPayload {
+  nome: string
+  descricao: string
+  arquivo: string
+  link: string
+  idCategoria: string
+  idProduto: string
+  tipoRenderizacao: string
+  ordem: number
+  padrao: boolean
+}
