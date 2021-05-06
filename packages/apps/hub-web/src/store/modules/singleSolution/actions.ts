0eff67b58d30fce7b02b8a069d6c442dd1ea6708
gabryelryba <gabryel.ryba@gmail.com>
2021-05-06 13:52:59 -0300

    refactor(store): remocao de actions e sagas que nao serao utilizadas

--- packages/apps/hub-web/src/store/modules/singleSolution/actions.ts
+++ packages/apps/hub-web/src/store/modules/singleSolution/actions.ts
@@ -1,51 +1,14 @@
 import { Action } from 'redux'
 
-import {
-  GetSolutionBySlugPayload,
-  PostSolutionPayload,
-  Solution
-} from './types'
+import { PostSolutionPayload } from './types'
 
 export const Actions = {
-  SOLUTION_GET_REQUEST: '@solution/SOLUTION_GET_REQUEST',
-  SOLUTION_GET_SUCCESS: '@solution/SOLUTION_GET_SUCCESS',
-  SOLUTION_GET_FAILURE: '@solution/SOLUTION_GET_FAILURE',
   SOLUTION_POST_REQUEST: '@solution/SOLUTION_POST_REQUEST',
   SOLUTION_POST_SUCCESS: '@solution/SOLUTION_POST_SUCCESS',
   SOLUTION_POST_FAILURE: '@solution/SOLUTION_POST_FAILURE',
   SOLUTION_DELETE_REQUEST: '@solution/SOLUTION_DELETE_REQUEST',
   SOLUTION_DELETE_SUCCESS: '@solution/SOLUTION_DELETE_SUCCESS',
   SOLUTION_DELETE_FAILURE: '@solution/SOLUTION_DELETE_FAILURE'
-}
-
-export function solutionGetRequest({
-  slug,
-  profile,
-  educationalLevel
-}: GetSolutionBySlugPayload): Action {
-  return {
-    type: Actions.SOLUTION_GET_REQUEST,
-    payload: {
-      slug,
-      profile,
-      educationalLevel
-    }
-  }
-}
-
-export function solutionGetFailure(): Action {
-  return {
-    type: Actions.SOLUTION_GET_FAILURE
-  }
-}
-
-export function solutionGetSuccess(solution: Solution): Action {
-  return {
-    type: Actions.SOLUTION_GET_SUCCESS,
-    payload: {
-      solution
-    }
-  }
 }
 
 export function solutionPostRequest(solution: PostSolutionPayload): Action {
--- packages/apps/hub-web/src/store/modules/singleSolution/sagas.ts
+++ packages/apps/hub-web/src/store/modules/singleSolution/sagas.ts
@@ -6,41 +6,14 @@ import { Action } from 'redux'
 import { toast } from '@psdhub/common/utils'
 import api from '@psdhub/api'
 
-import { Solution } from './types'
 import {
   Actions,
-  solutionGetFailure,
-  solutionGetSuccess,
   solutionPostSuccess,
   solutionPostFailure,
   solutionDeleteSuccess,
   solutionDeleteFailure
 } from './actions'
 import { loading } from '../global/actions'
-
-export function* getSolutionBySlug(action: Action): Generator {
-  yield put(loading(true))
-  // revisar pois o perfil sera sempre admin!!!!
-  const response = yield call(() => {
-    return api.get('Solucao/SolucaoPorSlug', {
-      params: {
-        slug: action.payload.slug,
-        perfil: action.payload.profile,
-        nivelEnsino: action.payload.educationalLevel
-      }
-    })
-  })
-
-  const { ok, data } = response as ApiResponse<Solution>
-
-  yield put(loading(false))
-
-  if (!ok || !data) {
-    toast.error('Erro ao buscar solução!')
-    return yield put(solutionGetFailure())
-  }
-  return yield put(solutionGetSuccess(data))
-}
 
 export function* createSolution(action: Action): Generator {
   yield put(loading(true))
@@ -84,7 +57,6 @@ export function* deleteSolution(action: Action): Generator {
 }
 
 export default all([
-  takeLatest(Actions.SOLUTION_GET_REQUEST, getSolutionBySlug),
   takeLatest(Actions.SOLUTION_POST_REQUEST, createSolution),
   takeLatest(Actions.SOLUTION_DELETE_REQUEST, deleteSolution)
 ])
