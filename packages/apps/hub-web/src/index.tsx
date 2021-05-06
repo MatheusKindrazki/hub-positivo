42e8f411f3ad659ddd477cd65623d2a84f35e59f
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-05-05 21:36:48 -0300

    refactor: refatorando disparo do GSC OnSubmit

--- packages/apps/hub-web/src/index.tsx
+++ packages/apps/hub-web/src/index.tsx
@@ -6,18 +6,17 @@ import mixpanel from 'mixpanel-browser'
 import getSiteControlInit from '@psdhub/gsc'
 
 import history from '~/services/history'
+import trackWidgetSubmit from '~/services/getSiteControl/trackWidgetSubmit'
 
 import mixPanelInit from './services/mixpanel/init'
 import hotjarInit from './services/hotjar/hotjarInit'
-import setGSCOnSubmit from './services/getSiteControl/gscOnSubmit'
 import App from './App'
 
 window.mixpanel = mixpanel
 
 hotjarInit()
 mixPanelInit()
-setGSCOnSubmit()
-getSiteControlInit(history)
+getSiteControlInit(history, trackWidgetSubmit)
 
 window.newrelic?.addRelease('@hub', process.env.REACT_APP_VERSION as string)
 
--- packages/gsc/src/index.ts
+++ packages/gsc/src/index.ts
@@ -1,8 +1,19 @@
 import { History, LocationState } from 'history'
 
-import { instanceGsc, removeGsc, setParamsGsc, pageViewGsc } from './services'
+import delay from '@psdhub/common/utils/delay'
+
+import {
+  instanceGsc,
+  removeGsc,
+  setParamsGsc,
+  pageViewGsc,
+  onSubmitGsc
+} from './services'
 
 type OrquestradorProps = History<LocationState>
+
+// onSubmitCallBack
+type CBProps = (id: number, data: any) => void
 
 let historyLength = 0
 window.pageviewCount = 0
@@ -12,16 +23,25 @@ const setCount = (): void => {
   window.pageviewCount = historyLength
 }
 
-function gscOrquestrador(history: OrquestradorProps): void {
-  window.onload = function () {
+function gscOrquestrador(history: OrquestradorProps, cb: CBProps): void {
+  const pathname = document.location.hash.replace('#', '')
+
+  window.onload = async function () {
     setCount()
     instanceGsc()
+
+    await delay(500)
+
+    pageViewGsc(pathname)
+
+    onSubmitGsc(cb)
   }
 
   history.listen(async e => {
     await removeGsc(() => instanceGsc())
 
     pageViewGsc(e.pathname)
+    onSubmitGsc(cb)
 
     setCount()
   })
