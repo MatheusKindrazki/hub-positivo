122e470949aac45380b9b71c9d68a8496a828065
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-05-11 22:53:06 -0300

    feat: adicionando logo on

--- packages/apps/easy-auth/src/components/Header/index.tsx
+++ packages/apps/easy-auth/src/components/Header/index.tsx
@@ -2,20 +2,42 @@ import React from 'react'
 
 import { Box } from '@psdhub/common/components'
 
+import GlobalStyle from './styles'
+import Logo from '../Logo'
+import { useAuth } from '../../context/authContext'
+
 const Header: React.FC = () => {
+  const { signed } = useAuth()
+
+  if (!signed) return null
+
   return (
-    <Box
-      pos="absolute"
-      top="0"
-      left="0"
-      zIndex="overlay"
-      w="100%"
-      h="72px"
-      borderBottom="1px solid"
-      borderBottomColor="gray.300"
-    >
-      brasil
-    </Box>
+    <>
+      <Box
+        pos="absolute"
+        top="-73px"
+        left="0"
+        zIndex="overlay"
+        w="100%"
+        h="72px"
+        bg="white"
+        p="4"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        borderBottom="1px solid #D9D9D9"
+      >
+        <Box
+          className="hub-logo"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Logo />
+        </Box>
+      </Box>
+      <GlobalStyle />
+    </>
   )
 }
 
---
+++ packages/apps/easy-auth/src/components/Header/styles.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  body {
+    margin-top: 73px!important;
+    background-color: #ECEFF1;
+  }
+`
