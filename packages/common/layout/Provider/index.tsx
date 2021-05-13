62bc4ab24aa4628055ba6b93648a900034089c21
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-05-13 19:35:21 -0300

    chore: atualizando yarn.lock

--- packages/common/layout/Provider/index.tsx
+++ packages/common/layout/Provider/index.tsx
@@ -41,8 +41,6 @@ const ThemeContainer: React.FC<ThemeProps> = ({ children, cssVarPrefix }) => {
     return hubThemeProfile
   }, [cssVarPrefix, prof])
 
-  console.log(renderTheme)
-
   return (
     <CacheProvider value={createCache({ key: cssKey })}>
       <ChakraProvider theme={renderTheme} resetCSS>
