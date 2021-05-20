d4a2cab5327a805d4f80c035294acda43a1a2f05
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-05-19 22:12:06 -0300

    build: atualizando select

--- packages/common/components/Select/variants/Normal.tsx
+++ packages/common/components/Select/variants/Normal.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Select, { Props, mergeStyles, NonceProvider } from 'react-select'
+import Select, { Props, mergeStyles } from 'react-select'
 
 import { useTheme } from '@psdhub/common/layout/styles'
 
@@ -21,22 +21,20 @@ const HubSelect = forwardRef<PropsSelect, 'select'>((props, ref) => {
   const theme = useTheme()
 
   return (
-    <NonceProvider cacheKey={`${prefixClass}-normal`} nonce="normal">
-      <Select
-        ref={ref}
-        isMulti={isMulti}
-        styles={mergeStyles(
-          { ...normal({ theme, inputHeight, error }) },
-          { ...styles }
-        )}
-        clearable
-        noOptionsMessage={() => 'Nada encontrado =('}
-        className={className}
-        classNamePrefix={`${prefixClass}-normal`}
-        isSearchable
-        {...rest}
-      />
-    </NonceProvider>
+    <Select
+      ref={ref}
+      isMulti={isMulti}
+      styles={mergeStyles(
+        { ...normal({ theme, inputHeight, error }) },
+        { ...styles }
+      )}
+      clearable
+      noOptionsMessage={() => 'Nada encontrado =('}
+      className={className}
+      classNamePrefix={`${prefixClass}-normal`}
+      isSearchable
+      {...rest}
+    />
   )
 })
 
