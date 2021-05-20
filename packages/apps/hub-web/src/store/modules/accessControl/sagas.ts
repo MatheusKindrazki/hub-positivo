deffac556368c37e3ea7a3b25c6144b98cea5cf0
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-05-20 12:17:02 -0300

    refactor: refatorar saga de put/post do controle de acessos para edição e criação de solução

--- packages/apps/hub-web/src/store/modules/accessControl/sagas.ts
+++ packages/apps/hub-web/src/store/modules/accessControl/sagas.ts
@@ -1,9 +1,10 @@
-import { takeLatest, all, Payload, put, call } from 'redux-saga/effects'
+import { takeLatest, all, put, call } from 'redux-saga/effects'
 
 import history from '~/services/history'
 
 import { AccessControlPutData, AccessControlPostData } from './types'
 import { Actions } from './actions'
+import { PostSolutionData, PutSolutionData } from '../solutions/types'
 import { createSolution, updateSolution } from '../solutions/sagas'
 import { solutionPostRequest, solutionPutRequest } from '../solutions/actions'
 import {
@@ -15,53 +16,39 @@ import {
   schoolPermissionsRequest
 } from '../permissions/actions'
 import { loading } from '../global/actions'
+interface SubmitSolution {
+  payload: AccessControlPostData | AccessControlPutData
+  type: string
+}
 
-type PostAccessControlPayload = Payload<AccessControlPostData>
-
-export function* submitPostAccessControl({
-  payload
-}: PostAccessControlPayload): Generator {
+export function* submitSolution({ payload, type }: SubmitSolution): Generator {
   const { solution, profilePermissions, schoolPermissions } = payload
-  yield put(loading(true))
 
-  // cria solucao e aguarda a finalizacao para criar permissoes e restricoes
-  const id = (yield call(
-    createSolution,
-    solutionPostRequest(solution)
-  )) as string
-
-  if (id) {
+  if (type === Actions.ACCESS_CONTROL_POST_REQUEST) {
+    const id = (yield call(
+      createSolution,
+      solutionPostRequest(solution as PostSolutionData)
+    )) as string
     profilePermissions.create.idSolucao = id
     profilePermissions.remove.idSolucao = id
     schoolPermissions.create.idSolucao = id
     schoolPermissions.remove.idSolucao = id
-    all([
-      yield call(profileSaga, profilePermissionsRequest(profilePermissions)),
-      yield call(schoolSaga, schoolPermissionsRequest(schoolPermissions))
-    ])
   }
-  yield put(loading(false))
-  history.push('/controle-de-acessos')
-}
 
-type PutAccessControlPayload = Payload<AccessControlPutData>
-
-export function* submitPutAccessControl({
-  payload
-}: PutAccessControlPayload): Generator {
-  const { solution, profilePermissions, schoolPermissions } = payload
-  yield put(loading(true))
+  if (type === Actions.ACCESS_CONTROL_PUT_REQUEST) {
+    yield call(updateSolution, solutionPutRequest(solution as PutSolutionData))
+  }
 
   all([
-    yield call(updateSolution, solutionPutRequest(solution)),
     yield call(profileSaga, profilePermissionsRequest(profilePermissions)),
     yield call(schoolSaga, schoolPermissionsRequest(schoolPermissions))
   ])
+
   yield put(loading(false))
   return yield history.push('/controle-de-acessos')
 }
 
 export default all([
-  takeLatest(Actions.ACCESS_CONTROL_PUT_REQUEST, submitPutAccessControl),
-  takeLatest(Actions.ACCESS_CONTROL_POST_REQUEST, submitPostAccessControl)
+  takeLatest(Actions.ACCESS_CONTROL_PUT_REQUEST, submitSolution),
+  takeLatest(Actions.ACCESS_CONTROL_POST_REQUEST, submitSolution)
 ])
