841d82a5c79f9bc74e5948d68c9726671e50c8a8
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-06-11 11:13:12 -0300

    chore: abstração do system

---
+++ packages/common/components/Tree/index.tsx
@@ -0,0 +1,29 @@
+// import React, { useMemo } from 'react';
+
+// // import { Container } from './styles';
+
+// const Tree: React.FC = () => {
+//   // renderOptions = (items = [], level = 0) => {
+//   //     level++
+//   //     return items.map(item => {
+//   //       return [
+//   //         <option
+//   //           disabled={item.disabled && item.disabled === true}
+//   //           className={`${this.props.className}__option-${level}`}
+//   //           key={item.id}
+//   //           value={item.id}
+//   //         >
+//   //           {this.props.spaceSymbol.repeat(level)}
+//   //           {item.name}
+//   //         </option>,
+//   //         item.children && item.children.length
+//   //           ? this.renderOptions(item.children, level)
+//   //           : null
+//   //       ]
+//   //     })
+//   //   }
+
+//   return <div />;
+// }
+
+// export default Tree;
---
+++ packages/common/components/Tree/utils/renderOptions.ts
@@ -0,0 +1,24 @@
+// interface RenderOptionsProps {
+//   items: OptionsProps[]
+//   level: number
+// }
+
+// interface OptionsProps {
+//   label: string
+//   value: string
+//   options?: OptionsProps[]
+// }
+
+// const renderOptions = (data: RenderOptionsProps) => {
+
+//   const { items, level } = data
+
+//   level++
+
+//   return items?.map(item => {
+//     return [
+
+//     ]
+//   })
+
+// }
