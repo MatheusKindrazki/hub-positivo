745c412b5c4b554b1b568987ad67672f40a11c84
gabryelryba <gabryel.ryba@gmail.com>
2021-06-11 17:45:38 -0300

    refactor(footer): remocao do footer das telas de auth

--- packages/apps/web/src/layouts/Auth/index.tsx
+++ packages/apps/web/src/layouts/Auth/index.tsx
@@ -2,31 +2,26 @@ import React from 'react'
 
 import { useSelector } from 'react-redux'
 
-import Footer from '@psdhub/common/components/Footer'
 import { BarLoader, CardBox, Text } from '@psdhub/common/components'
 
 import Logo from '~/components/Logo'
 
 import { Container } from './styles'
-import footerData from '../Logged/footerData.json'
 
 const Auth: React.FC = ({ children }) => {
   const { loading } = useSelector((state: Store.State) => state.global)
 
   return (
-    <>
-      <Container>
-        <BarLoader height="4px" loading={loading} />
-        <Logo />
-        <CardBox mt="2.1875rem" maxWidth="25.9375rem">
-          {children}
-        </CardBox>
-        <Text mt="4" color="gray.500">
-          v{process.env.REACT_APP_VERSION}
-        </Text>
-      </Container>
-      <Footer columns={footerData} />
-    </>
+    <Container>
+      <BarLoader height="4px" loading={loading} />
+      <Logo />
+      <CardBox mt="2.1875rem" maxWidth="25.9375rem">
+        {children}
+      </CardBox>
+      <Text mt="4" color="gray.500">
+        v{process.env.REACT_APP_VERSION}
+      </Text>
+    </Container>
   )
 }
 
