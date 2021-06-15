543a5447769a0631c458894c08d5ad3fc5556b20
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-06-15 10:27:33 -0300

    style: alterar estilos/padding do footer

--- packages/common/components/Footer/components/FooterLogo/index.tsx
+++ packages/common/components/Footer/components/FooterLogo/index.tsx
@@ -7,9 +7,9 @@ import { BoxProps } from '../../../Box'
 const FooterLogo: React.FC<BoxProps> = props => {
   return (
     <Box
-      maxW="1400px"
-      w={['90%']}
       m="auto"
+      px="4"
+      maxW="1400px"
       {...props}
       d="flex"
       flexDir={['column', 'row']}
