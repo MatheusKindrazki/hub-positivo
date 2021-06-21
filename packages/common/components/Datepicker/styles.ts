2741084bcfe40678b3bf7009391855f9e38c8558
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-06-21 14:52:52 -0300

    style: deixar css em ordem alfabética e em cascata para facilitação da leitura

--- packages/common/components/Datepicker/styles.ts
+++ packages/common/components/Datepicker/styles.ts
@@ -3,14 +3,42 @@ import styled from 'styled-components'
 import { Box } from '..'
 
 const Container = styled(Box)`
+  .datepicker-calendar {
+    border: 0.0625rem solid ${({ theme }) => theme.colors.gray[400]};
+    border-radius: 0.5rem;
+    margin: auto;
+  }
+
+  .react-datepicker__current-month {
+    color: ${({ theme }) => theme.colors.black[600]};
+    font-family: 'TTNORMS';
+    font-size: 1rem;
+    font-weight: 700;
+  }
+
+  .react-datepicker__header {
+    background: white;
+    border: 0;
+  }
+
+  .react-datepicker__navigation-icon {
+    ::before {
+      border-color: ${({ theme }) => theme.colors.blue[500]};
+      top: 0.9375rem;
+    }
+  }
+
+  .react-datepicker__week {
+    margin: 0.05rem;
+  }
+
   .react-datepicker__day {
     border-radius: 0;
     font-family: 'TTNORMS';
     font-weight: 400;
-    width: 2.5rem;
     line-height: 2.5rem;
     margin: 0;
-
+    width: 2.5rem;
     &:not(.react-datepicker__day--in-selecting-range, .react-datepicker__day--in-range):hover {
       border-radius: 0.5rem !important;
     }
@@ -29,8 +57,8 @@ const Container = styled(Box)`
   }
 
   .react-datepicker__day--in-range {
-    color: white;
     background: ${({ theme }) => theme.colors.blue[500]};
+    color: white;
   }
 
   .react-datepicker__day--selecting-range-start {
@@ -39,9 +67,6 @@ const Container = styled(Box)`
 
   .react-datepicker__day--selected {
     border-radius: 0.5rem;
-    /* &:hover {
-      border-radius: 0.5rem !important;
-    } */
     .react-datepicker__day--selecting-range-start {
       border-radius: 0.5rem 0 0 0.5rem !important;
     }
@@ -49,17 +74,6 @@ const Container = styled(Box)`
 
   .react-datepicker__day--in-selecting-range:hover {
     border-radius: 0 0.5rem 0.5rem 0 !important;
-  }
-
-  .react-datepicker__week {
-    margin: 0.05rem;
-  }
-
-  .react-datepicker__navigation-icon {
-    ::before {
-      border-color: ${({ theme }) => theme.colors.blue[500]};
-      top: 0.9375rem;
-    }
   }
 
   .react-datepicker__day--outside-month {
@@ -71,30 +85,12 @@ const Container = styled(Box)`
   }
 
   .react-datepicker__day-name {
-    margin: 0;
-    width: 2.5rem;
-    height: 2.5rem;
-    text-align: center;
-    line-height: 2.5rem;
     color: ${({ theme }) => theme.colors.gray[500]};
-  }
-
-  .datepicker-calendar {
-    margin: auto;
-    border: 0.0625rem solid ${({ theme }) => theme.colors.gray[400]};
-    border-radius: 0.5rem;
-  }
-
-  .react-datepicker__header {
-    border: 0;
-    background: white;
-  }
-
-  .react-datepicker__current-month {
-    font-family: 'TTNORMS';
-    font-weight: 700;
-    color: ${({ theme }) => theme.colors.black[600]};
-    font-size: 1rem;
+    height: 2.5rem;
+    line-height: 2.5rem;
+    margin: 0;
+    text-align: center;
+    width: 2.5rem;
   }
 `
 
