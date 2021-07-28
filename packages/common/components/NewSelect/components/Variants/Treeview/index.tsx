0904488213990f6f6d2119122b20631649ed7d17
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-07-28 10:22:38 -0300

    feat: adicionando search nos componentes de checkbox

--- packages/common/components/NewSelect/components/Variants/Treeview/index.tsx
+++ packages/common/components/NewSelect/components/Variants/Treeview/index.tsx
@@ -1,25 +1,43 @@
-import React, { useContext, useMemo } from 'react'
+import React, { useContext, useMemo, useState } from 'react'
 
 import Tree from '@psdhub/common/components/Tree'
 
 import { ContainerOptions } from './styles'
+import NotFound from '../../NotFound'
+import { filterRecursive } from '../../../utils'
+import { TreeNode } from '../../../types'
 import SelectContext from '../../../context'
 
 const DefaultVariant: React.FC = props => {
-  const { options, state, onChange } = useContext(SelectContext)
+  const context = useContext(SelectContext)
+
+  const { options, state, onChange, noOptionsMessage } = context
+
+  const [renderedOptions, setRenderedOptions] = useState(options)
 
   const renderDefaultOptions = useMemo(() => {
     return state.checked
   }, [state.checked])
 
+  context.searchable = (e: string) => {
+    if (!e) return setRenderedOptions(options)
+
+    const filteredOptions = filterRecursive<TreeNode>(options, e)
+    setRenderedOptions(filteredOptions)
+  }
+
   return (
     <ContainerOptions>
-      <Tree
-        {...props}
-        options={options}
-        defaultOptions={renderDefaultOptions}
-        onChange={onChange}
-      />
+      {renderedOptions?.length ? (
+        <Tree
+          {...props}
+          options={renderedOptions}
+          defaultOptions={renderDefaultOptions}
+          onChange={onChange}
+        />
+      ) : (
+        <NotFound noOptionsMessage={noOptionsMessage as React.FC} />
+      )}
     </ContainerOptions>
   )
 }
