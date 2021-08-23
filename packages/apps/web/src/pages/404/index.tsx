446e39f680c19e185ba1e3d8c60beab370c1aa55
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-08-23 20:40:28 -0300

    feat: criando pagina 404

--- packages/apps/web/src/pages/404/index.tsx
+++ packages/apps/web/src/pages/404/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
+import { Image, Button } from '@psdhub/common/components'
+
 import { Container } from './styles'
+import notFoundIcon from './404.svg'
 
 const NotFound: React.FC = () => {
   return (
     <Container>
-      <h1>404</h1>
+      <Image src={notFoundIcon} alt="Icon de Página não encontrada" />
+      <Button
+        as={Link}
+        to="/"
+        mt="12"
+        colorScheme="blue"
+        w="100%"
+        maxW="250px"
+        h="50px"
+      >
+        Voltar para a home
+      </Button>
     </Container>
   )
 }
--- packages/apps/web/src/pages/404/styles.ts
+++ packages/apps/web/src/pages/404/styles.ts
@@ -3,7 +3,20 @@ import styled from 'styled-components'
 import { Box } from '@psdhub/common/components'
 
 export const Container = styled(Box)`
-  min-height: 100vh;
+  min-height: calc(100vh - 415px);
   width: 100%;
-  background: red;
+
+  padding: 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  margin-top: 20px;
+
+  @media (min-width: 576px) {
+    margin-top: 60px;
+    padding: 0;
+  }
 `
