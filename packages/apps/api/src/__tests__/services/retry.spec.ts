ba86b44431428b0db54ddfca5bf8e92e32ef7189
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-08-24 20:30:03 -0300

    test: aumentando timeout do teste

--- packages/apps/api/src/__tests__/services/retry.spec.ts
+++ packages/apps/api/src/__tests__/services/retry.spec.ts
@@ -6,8 +6,6 @@ import apiRetry from '../../services/retry'
 
 describe('Api retry', () => {
   it('should ignore the retry if the API status is different from expected', async () => {
-    jest.setTimeout(12000)
-
     apiRetry(axios)
 
     const mock = new MockAdapter(axios)
@@ -21,6 +19,25 @@ describe('Api retry', () => {
 
       expect(response.status).toBe(400)
       expect(responseConfig.retryCount).toBe(0)
+    }
+  })
+
+  it('Should try 3 times communication with the API if the status is expected', async () => {
+    jest.setTimeout(12000)
+
+    apiRetry(axios)
+
+    const mock = new MockAdapter(axios, { delayResponse: 0 })
+    mock.onGet('/').reply(503)
+
+    try {
+      await axios.get('/')
+    } catch (error) {
+      const { response } = error
+      const responseConfig = response.config['axios-retry'] as any
+
+      expect(response.status).toBe(503)
+      expect(responseConfig.retryCount).toBe(3)
     }
   })
 })
