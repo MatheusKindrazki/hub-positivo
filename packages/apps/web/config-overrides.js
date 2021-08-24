314848e6e692d10bf8991c15c3f596dc41c6b1d2
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-08-24 13:39:28 -0300

    chore: atualizando overrides para inclusão da API

--- packages/apps/web/config-overrides.js
+++ packages/apps/web/config-overrides.js
@@ -9,6 +9,7 @@ module.exports = function (config, env) {
       babelInclude([
         path.resolve('src'),
         path.resolve('../../common'),
+        path.resolve('../api'),
         path.resolve('../gsc')
       ]),
       addBabelPlugin([
