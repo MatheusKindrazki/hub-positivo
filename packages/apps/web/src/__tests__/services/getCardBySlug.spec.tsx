e8f177615906d14992758c9dbfa1f60703adccdc
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-09-09 13:08:13 -0300

    test: refatorar testes do getCardBySlug com get instance da API

--- packages/apps/web/src/__tests__/services/getCardBySlug.spec.tsx
+++ packages/apps/web/src/__tests__/services/getCardBySlug.spec.tsx
@@ -1,9 +1,17 @@
 import { store } from '~/store'
 
 import { toast } from '@psdhub/common/utils'
-import api from '@psdhub/api'
+import * as api from '@psdhub/api'
 
 import { getCardBySlug } from '~/services/getCardBySlug'
+
+jest.mock('@psdhub/api', () => {
+  const rest = jest.requireActual('@psdhub/api')
+  return {
+    ...rest,
+    getInstance: jest.fn()
+  }
+})
 
 const mockedProps = {
   slug: 'test-slug',
@@ -21,7 +29,8 @@ const mockedApiResponse = {
 const mockedApiErrorResponse = {
   ok: false,
   data: {
-    test: true
+    test: true,
+    mensagem: 'Sinto muito, você não tem acesso a esta solução.'
   }
 }
 
@@ -30,26 +39,32 @@ const mockedGetParameters = {
   perfil: 'test-profile',
   slug: 'test-slug'
 }
-const mockedHeaders = { headers: { Authorization: 'Bearer test-token' } }
+const mockedHeaders = { headers: { Authorization: 'Bearer reduced-token' } }
 
 describe('getCardBySlug should work as expected', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
-
-  jest
-    .spyOn(store, 'getState')
-    .mockReturnValue({ auth: { token: 'test-token' } } as any)
+  jest.spyOn(store, 'getState').mockReturnValue({
+    auth: { reduced_token: 'reduced-token' }
+  } as any)
 
   it('getCardBySlug should work as expected', async () => {
     // mock da resposta da api
-    const getSpy = jest
-      .spyOn(api, 'get')
-      .mockResolvedValue(mockedApiResponse as any)
 
+    const spyGetInstance = jest.spyOn(api, 'getInstance').mockReturnValue({
+      get: jest.fn(() => mockedApiResponse),
+      setHeaders: jest.fn()
+    } as any)
+
+    const spyGet = jest.spyOn(api.getInstance('default'), 'get')
+    const spySetHeaders = jest.spyOn(api.getInstance('default'), 'setHeaders')
     const result = await getCardBySlug(mockedProps)
-
-    expect(getSpy).toHaveBeenCalledWith(
+    expect(spyGetInstance).toHaveBeenCalledWith('default')
+    expect(spySetHeaders).toHaveBeenCalledWith({
+      Authorization: 'Bearer reduced-token'
+    })
+    expect(spyGet).toHaveBeenCalledWith(
       'Solucao/SolucaoPorSlug',
       mockedGetParameters,
       mockedHeaders
@@ -59,21 +74,28 @@ describe('getCardBySlug should work as expected', () => {
 
   it('getCardBySlug should throw a toast error when api returns with ok false', async () => {
     const toastSpy = jest.spyOn(toast, 'error')
+
+    jest.spyOn(api, 'getInstance').mockReturnValue({
+      get: jest.fn(() => mockedApiResponse),
+      setHeaders: jest.fn()
+    } as any)
     // mock da resposta da api com ok: false
-    const getSpy = jest
-      .spyOn(api, 'get')
-      .mockResolvedValue(mockedApiErrorResponse as any)
+    const spyGet = jest
+      .spyOn(api.getInstance('default'), 'get')
+      .mockImplementation(() => mockedApiErrorResponse as any)
+
+    jest
+      .spyOn(api.getInstance('default'), 'setHeaders')
+      .mockImplementation(jest.fn())
 
     await getCardBySlug(mockedProps)
 
-    expect(getSpy).toHaveBeenCalledWith(
+    expect(spyGet).toHaveBeenCalledWith(
       'Solucao/SolucaoPorSlug',
       mockedGetParameters,
       mockedHeaders
     )
-    expect(toastSpy).toHaveBeenCalledWith(
-      'Sinto muito, você não tem acesso a esta solução.'
-    )
+    expect(toastSpy).toHaveBeenCalledWith(mockedApiErrorResponse.data.mensagem)
   })
 
   it('getCardBySlug should early return when no slug is received', async () => {
@@ -81,7 +103,12 @@ describe('getCardBySlug should work as expected', () => {
     mockedProps.slug = undefined as any
 
     // spyOn do get para verificacao da chamada
-    const getSpy = jest.spyOn(api, 'get')
+    const getSpy = jest.spyOn(api, 'getInstance').mockImplementation(
+      () =>
+        ({
+          get: jest.fn()
+        } as any)
+    )
 
     const result = await getCardBySlug(mockedProps)
 
