55500f72201d704cd7c43f7986069c418d0476ca
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-09-13 08:36:50 -0300

    test: testar communicator do MCF

---
+++ packages/apps/web/src/__tests__/pages/Solutions/pages/Microfrontend/communicator.spec.ts
@@ -0,0 +1,71 @@
+import * as helpers from '@psdhub/helpers'
+
+import communicatorMCF, {
+  clearData
+} from '~/pages/Solutions/pages/Microfrontend/communicator'
+
+jest.mock('@psdhub/helpers', () => {
+  const rest = jest.requireActual('@psdhub/helpers')
+  return {
+    ...rest,
+    getInformations: {
+      publish: jest.fn(),
+      clear: jest.fn()
+    }
+  }
+})
+describe('communicatorMCF should work properly', () => {
+  const data = {
+    auth: { reduced_token: 'reducedToken', token: 'token' },
+    profile: {
+      guid: 'guid',
+      colorProfile: 'colorProfile'
+    },
+    user: {
+      info: {
+        username: 'username',
+        email: 'email',
+        email_salas: 'email_salas',
+        name: 'name'
+      },
+      school: {
+        label: 'label',
+        roles: [],
+        user_id: 'user_id',
+        value: 'value'
+      }
+    },
+    educationalStage: {
+      class: 'class'
+    }
+  }
+  it('Should call publish function with correct params', () => {
+    communicatorMCF(data as any, { blue: {} } as any)
+    const spyPublish = jest.spyOn(helpers.getInformations, 'publish' as never)
+
+    const { profile, auth, educationalStage, user } = data
+
+    expect(spyPublish).toHaveBeenCalledWith({
+      logged_in: {
+        profile: profile.guid,
+        profileTheme: profile.colorProfile,
+        school: {
+          id: user.school.value,
+          name: user.school.label,
+          class: educationalStage.class
+        }
+      },
+      primary_color: {},
+      reduced_token: auth.reduced_token,
+      token: auth.token,
+      user_info: user.info
+    })
+  })
+})
+
+describe('clearData should work properly', () => {
+  it('', () => {
+    clearData()
+    expect(1).toBeTruthy()
+  })
+})
