6fe26ca88a8bda1a7afea0624708c60ea75feab3
gabryelryba <gabryel.ryba@gmail.com>
2021-09-17 13:37:26 -0300

    test: educationalLevelMenu

---
+++ packages/apps/web/src/__tests__/components/Header/EducationalLevelMenu.spec.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import { fireEvent } from '@testing-library/dom'
+
+import { setEducationalStage } from '~/store/modules/educationalStage/actions'
+import { store } from '~/store'
+
+import { render, waitFor } from '@psdhub/test-utils'
+
+import { EducationalLevelMenu } from '~/components/Header/components'
+
+describe('Header component should works as expected', () => {
+  const CUSTOM_STATE = {
+    profile: { name: 'Professor' },
+    educationalStage: {
+      levels: [
+        { label: 'level 1', value: '1' },
+        { label: 'level 2', value: '2' }
+      ],
+      level: '1'
+    }
+  }
+
+  const setup = (customState = CUSTOM_STATE) =>
+    render(<EducationalLevelMenu />, {
+      store,
+      reducers: ['educationalStage', 'profile'],
+      CUSTOM_STATE: customState
+    })
+
+  it('should render levels on screen', () => {
+    const levelLabels = ['level 1', 'level 2']
+
+    const { getByText } = setup()
+
+    levelLabels.forEach(label => expect(getByText(label)).toBeInTheDocument())
+  })
+  it('should dispatch an action when a level is clicked', async () => {
+    const { getByText, storeUtils } = setup()
+
+    await waitFor(() => fireEvent.click(getByText('level 1')))
+
+    expect(storeUtils?.getActions()).toContainEqual(setEducationalStage('1'))
+  })
+
+  it('should return null if no levels are found', async () => {
+    const newState = CUSTOM_STATE
+
+    newState.educationalStage.levels = []
+
+    const { queryByText } = setup()
+
+    expect(queryByText('level 1')).toBe(null)
+  })
+})
