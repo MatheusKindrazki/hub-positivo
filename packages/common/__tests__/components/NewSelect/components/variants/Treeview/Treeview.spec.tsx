f4dfde623fe6148151be5f18e144916f15c774e4
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-09-22 18:59:11 -0300

    test: renderizar treeview

--- packages/common/__tests__/components/NewSelect/components/variants/Treeview/Treeview.spec.tsx
+++ packages/common/__tests__/components/NewSelect/components/variants/Treeview/Treeview.spec.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { render } from '@psdhub/test-utils'
 
-// import * as context from '../../../components/NewSelect/context'
 import Treeview from '../../../../../../components/NewSelect/components/Variants/Treeview'
 
 describe('Treeview should work as expected', () => {
