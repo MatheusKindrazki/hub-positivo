3ffef6f09d0909b3ac1a7a8266b277c2a7659779
gabryelryba <gabryel.ryba@gmail.com>
2021-09-23 12:29:16 -0300

    fix: estilização mobile do animateGoBack

--- packages/apps/web/src/components/Header/components/AnimateGoBack/index.tsx
+++ packages/apps/web/src/components/Header/components/AnimateGoBack/index.tsx
@@ -25,11 +25,11 @@ const AnimateGoBack: React.FC<AnimateGoBackProps> = ({ onClick }) => {
       >
         <Box as={ArrowLeft} size={24} color="blue.500" />
       </Button>
-      <Box className="hub-svg-logo">
+      <Box w="16" className="hub-svg-logo">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="20%"
-          height="20%"
+          width="100%"
+          height="100%"
           viewBox="0 0 1045.49 543.6"
         >
           <g id="Layer_2" data-name="Layer 2">
