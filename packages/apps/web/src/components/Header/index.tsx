074f0733e944767a03ed43bd13a1dbf69b51b2b6
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-09-23 10:27:47 -0300

    refactor: remover menubar do header

--- packages/apps/web/src/components/Header/index.tsx
+++ packages/apps/web/src/components/Header/index.tsx
@@ -11,6 +11,7 @@ import {
 import { useMediaQuery } from '@psdhub/common/hooks'
 import { Box } from '@psdhub/common/components'
 
+<<<<<<< HEAD
 import EducationalLevelMenu from './components/EducationalLevelMenu/EducationalLevelMenu'
 import AnimateGoBack from './components/AnimateGoBack'
 import { SchoolLabel, HeaderButton } from './components'
@@ -20,6 +21,25 @@ export interface HeaderProps {
   handleGoBack: () => void | Promise<void>
   schoolName?: string
 }
+=======
+import { useMediaQuery, useDisclosure } from '@psdhub/common/hooks'
+import { Box, Button, Modal } from '@psdhub/common/components'
+
+import history from '~/services/history'
+
+import Logo from '~/components/LogoOn'
+
+import { HeaderProvider } from './context'
+import MobileMenu, { MenuButton, RefMenuProps } from './components/Mobile'
+import DesktopMenu from './components/Desktop'
+import AlterPass from './components/AlterPass'
+
+const Header: React.FC = () => {
+  const { signed } = useSelector((state: Store.State) => state.auth)
+  const { nobreak } = useSelector((state: Store.State) => state.noBreakAccess)
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+>>>>>>> refactor: remover menubar do header
 
 const Header: React.FC<HeaderProps> = ({ schoolName, handleGoBack }) => {
   const [isDesktop] = useMediaQuery('(min-width: 480px)')
@@ -31,6 +51,7 @@ const Header: React.FC<HeaderProps> = ({ schoolName, handleGoBack }) => {
   }, [])
 
   return (
+<<<<<<< HEAD
     <Headroom disable={isDesktop} style={{ zIndex: 2 }}>
       {schoolName && <SchoolLabel schoolName={schoolName} />}
       <Box
@@ -41,6 +62,10 @@ const Header: React.FC<HeaderProps> = ({ schoolName, handleGoBack }) => {
         justifyContent="center"
         zIndex={99999}
       >
+=======
+    <HeaderProvider>
+      <Headroom disable={isDesktop} style={{ zIndex: 2 }}>
+>>>>>>> refactor: remover menubar do header
         <Box
           width="100%"
           margin="0 auto"
