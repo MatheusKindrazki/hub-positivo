5405c7ee9b183d5f9d7537c120ecf9c778706f61
gabryelryba <gabryel.ryba@gmail.com>
2021-09-27 15:35:41 -0300

    test: actions do modulo de notificações da store

---
+++ packages/apps/web/src/__tests__/store/modules/notifications/actions.spec.ts
@@ -0,0 +1,54 @@
+import * as notificationsActions from '~/store/modules/notifications/actions'
+
+describe('Action of global history', () => {
+  it('Should call the notificationsRequest action correctly and receive the value within payload', () => {
+    const spy = jest.spyOn(notificationsActions, 'notificationsRequest')
+
+    const mockedType = {
+      type: '@notifications/GET_REQUEST'
+    }
+
+    const resolved = notificationsActions.notificationsRequest()
+
+    expect(spy).toBeCalled()
+
+    expect(resolved).toEqual({ ...mockedType })
+  })
+
+  it('Should call the notificationsSuccess action correctly and receive the value within payload', () => {
+    const spy = jest.spyOn(notificationsActions, 'notificationsSuccess')
+
+    const mockedType = {
+      type: '@notifications/GET_SUCCESS'
+    }
+
+    const mock = [
+      {
+        title: 'title',
+        message: 'message',
+        source: 'source',
+        date: new Date()
+      }
+    ]
+
+    const resolved = notificationsActions.notificationsSuccess([mock])
+
+    expect(spy).toBeCalled()
+
+    expect(resolved).toEqual({ ...mockedType, payload: [mock] })
+  })
+
+  it('Should call the notificationsFailure action correctly and receive the value within payload', () => {
+    const spy = jest.spyOn(notificationsActions, 'notificationsFailure')
+
+    const mockedType = {
+      type: '@notifications/GET_FAILURE'
+    }
+
+    const resolved = notificationsActions.notificationsFailure()
+
+    expect(spy).toBeCalled()
+
+    expect(resolved).toEqual({ ...mockedType })
+  })
+})
