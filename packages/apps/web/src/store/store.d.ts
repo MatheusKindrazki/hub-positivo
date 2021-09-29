87348862afd365b84c50f0c581f1096544cabc74
gabryelryba <gabryel.ryba@gmail.com>
2021-09-29 17:36:12 -0300

    refactor: utilizando dados da store no dropdown de notificações

--- packages/apps/web/src/store/store.d.ts
+++ packages/apps/web/src/store/store.d.ts
@@ -5,7 +5,7 @@ import { SchoolReducer } from './modules/school/types'
 import { ProfileReducer } from './modules/profile/types'
 import { ProductReducer } from './modules/products/types'
 import { PermissionsReducer } from './modules/permissions/types'
-import { NotificationReducer } from './modules/notifications/types'
+import { NotificationsReducer } from './modules/notifications/types'
 import { NoBreakReducer } from './modules/noBreakAccess/types'
 import { ClassesReducer } from './modules/myClasses/types'
 import { GlobalReducer } from './modules/global/types'
@@ -34,6 +34,7 @@ interface ApplicationState {
   acceptTerms: AcceptTermsState
   notifications: NotificationReducer
   acceptTerms: AcceptTermsState
+  notifications: NotificationsReducer
 }
 declare global {
   declare namespace Store {
