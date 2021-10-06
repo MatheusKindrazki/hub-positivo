837303e1eb21f39228c4a518b7a692b8459e5a29
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-10-06 10:19:30 -0300

    fix: comentando modulo de notificação

--- packages/apps/web/src/store/modules/notifications/sagas.ts
+++ packages/apps/web/src/store/modules/notifications/sagas.ts
@@ -1,37 +1,37 @@
-import { ApiResponse } from 'apisauce'
+// import { ApiResponse } from 'apisauce'
 
-import { all, call, put, takeLatest } from 'redux-saga/effects'
+import { all, put, takeLatest } from 'redux-saga/effects'
 
-import { loading } from '~/store/modules/global/actions'
-import { store } from '~/store'
+// import { loading } from '~/store/modules/global/actions'
+// import { store } from '~/store'
 
-import { fakeNotificationApi } from '@psdhub/web/src/__mocks__/api/fakeNotificationsApi'
-import { toast } from '@psdhub/common/utils'
+// import { fakeNotificationApi } from '@psdhub/web/src/__mocks__/api/fakeNotificationsApi'
+// import { toast } from '@psdhub/common/utils'
 
-import { NotificationHistory } from './types'
-import { Actions, notificationsFailure, notificationsSuccess } from './actions'
+// import { NotificationHistory } from './types'
+import { Actions, notificationsFailure } from './actions'
 export function* getNotifications(): Generator {
-  const { school } = store.getState().user
-  const { reduced_token } = store.getState().auth
+  // const { school } = store.getState().user
+  // const { reduced_token } = store.getState().auth
 
-  if (!school?.user_id || !reduced_token) {
-    return yield put(notificationsFailure())
-  }
+  // if (!school?.user_id || !reduced_token) {
+  // }
+  return yield put(notificationsFailure())
 
-  yield put(loading(true))
+  // yield put(loading(true))
 
-  const failChance = Math.random() > 0.9
-  const response = yield call(() => fakeNotificationApi(failChance))
+  // const failChance = Math.random() > 0.9
+  // const response = yield call(() => fakeNotificationApi(failChance))
 
-  const { ok, data } = response as ApiResponse<NotificationHistory[]>
-  yield put(loading(false))
+  // const { ok, data } = response as ApiResponse<NotificationHistory[]>
+  // yield put(loading(false))
 
-  if (!ok || !data) {
-    toast.error('Erro ao buscar notificações!')
-    return yield put(notificationsFailure())
-  }
+  // if (!ok || !data) {
+  //   toast.error('Erro ao buscar notificações!')
+  //   return yield put(notificationsFailure())
+  // }
 
-  return yield put(notificationsSuccess(data))
+  // return yield put(notificationsSuccess(data))
 }
 
 export default all([takeLatest(Actions.GET_REQUEST, getNotifications)])
