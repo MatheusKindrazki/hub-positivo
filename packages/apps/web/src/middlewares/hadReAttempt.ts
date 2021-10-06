e40a8d6ef0e1347bd06395077421fe833e2d72ba
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-10-06 18:03:49 -0300

    refactor: adicionando tratamento de excessões dentro do retry

--- packages/apps/web/src/middlewares/hadReAttempt.ts
+++ packages/apps/web/src/middlewares/hadReAttempt.ts
@@ -1,4 +1,4 @@
-import { pageAction } from '@psdhub/newrelic'
+import { pageAction, noticeError } from '@psdhub/newrelic'
 import { communicationURLs, Variant, getInstance } from '@psdhub/api'
 interface HadReAttempt {
   attempt?: number
@@ -16,19 +16,23 @@ Object.keys(communicationURLs).forEach(key => {
   const instanceKey = key as Variant
 
   getInstance(instanceKey).addResponseTransform(transform => {
-    const { status, config, originalError } = transform
+    try {
+      const { status, config, originalError } = transform
 
-    const retry = config?.['axios-retry'] as any
+      const retry = config?.['axios-retry'] as any
 
-    if (retry?.retryCount === 0) return
+      if (retry?.retryCount === 0) return
 
-    hadReAttempt({
-      attempt: retry.retryCount as number,
-      status: status,
-      baseURL: config?.baseURL,
-      endpoint: config?.url,
-      error: originalError?.message
-    })
+      hadReAttempt({
+        attempt: retry.retryCount as number,
+        status: status,
+        baseURL: config?.baseURL,
+        endpoint: config?.url,
+        error: originalError?.message
+      })
+    } catch (error) {
+      noticeError(error as Error)
+    }
   })
 })
 
