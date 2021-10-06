9e4ab59dff12a3006f4a4cd8015c1177b6256e55
Lucas Cardoso <lsc.cardosoo@gmail.com>
2021-10-06 11:19:30 -0300

    refactor: remover code smells do style de dropzone

--- packages/common/components/Dropzone/styles.ts
+++ packages/common/components/Dropzone/styles.ts
@@ -13,15 +13,21 @@ interface PreviewContainerProps {
   colors: any
 }
 
+const validUrl = (props: ContainerProps) =>
+  props.url
+    ? `2px solid ${props.colors.gray[100]};`
+    : `2px dashed ${props.colors.gray[100]};`
+
+const redBorder = (props: ContainerProps) =>
+  `2px solid ${props.colors.red[300]};`
+
+const containerBorder = (props: ContainerProps) =>
+  props?.reject || props?.error ? redBorder(props) : validUrl(props)
+
 export const Container = styled(Box)<ContainerProps>`
   display: flex;
   padding: 1rem;
-  border: ${props =>
-    props?.reject || props?.error
-      ? `2px solid ${props.colors.red[300]};`
-      : props.url
-      ? `2px solid ${props.colors.gray[100]};`
-      : `2px dashed ${props.colors.gray[100]};`};
+  border: ${props => containerBorder(props)};
   border-radius: 8px;
   box-sizing: border-box;
   transition: all 0.2s linear;
@@ -31,7 +37,7 @@ export const Container = styled(Box)<ContainerProps>`
     cursor: pointer;
 
     &:hover {
-      border: ${props => `2px dashed ${props.colors.blue[300]};`};
+      border: ${props => '2px dashed' + props.colors.blue[300]};
     }
   }
 `
