1eed39b1712057e54b55123b3aed5b6cea1d2bf1
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-10-13 13:28:15 -0300

    feat: adicionando informativo caso o usuario não aceite o termo

--- packages/apps/web/src/components/ModalAcceptTerms/index.tsx
+++ packages/apps/web/src/components/ModalAcceptTerms/index.tsx
@@ -1,16 +1,24 @@
-import React, { useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
 
 import { signOut } from '~/store/modules/auth/actions'
+import { acceptTermsRequest } from '~/store/modules/acceptTerms/actions'
 
 import { useDisclosure, useMediaQuery } from '@psdhub/common/hooks'
 import Modal from '@psdhub/common/components/Modal'
+import {
+  Alert,
+  AlertIcon,
+  AlertDescription
+} from '@psdhub/common/components/Alert'
 import { Box, Button, Text, Checkbox } from '@psdhub/common/components'
 
 import GlobalStyles from './styles'
 
 const ModalAcceptTerms: React.FC = () => {
+  const [accept, setAccept] = useState(false)
+
   const dispatch = useDispatch()
 
   const { accepted, checking } = useSelector(
@@ -27,9 +35,13 @@ const ModalAcceptTerms: React.FC = () => {
 
   const [isDesktop] = useMediaQuery('(min-width: 480px)')
 
-  const handleSignOut = useCallback(() => {
-    dispatch(signOut())
-  }, [dispatch])
+  const handleAcceptTerms = useCallback(() => {
+    if (!accept) {
+      return dispatch(signOut())
+    }
+
+    dispatch(acceptTermsRequest())
+  }, [accept, dispatch])
 
   return (
     <>
@@ -67,19 +79,33 @@ const ModalAcceptTerms: React.FC = () => {
             política de privacidade
           </Button>
 
-          <Checkbox size="lg" my="1rem" defaultChecked>
+          <Checkbox
+            size="lg"
+            my="1rem"
+            isChecked={accept}
+            onChange={() => setAccept(!accept)}
+          >
             <Text fontSize="0.9375rem">
               Tenho mais de 18 anos, li e aceito os termos do Aviso de
               Privacidade e Política de Privacidade
             </Text>
           </Checkbox>
 
+          {!accept && (
+            <Alert status="warning" my="1">
+              <AlertIcon />
+              <AlertDescription fontSize=".8rem">
+                Você será desconectado caso não aceite os termos.
+              </AlertDescription>
+            </Alert>
+          )}
+
           <Button
             my="1rem"
             colorScheme="blue"
             w="100%"
             h="48px"
-            onClick={handleSignOut}
+            onClick={handleAcceptTerms}
           >
             CONCLUIR
           </Button>
