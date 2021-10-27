52e18e8e86363f91e8ddadd8604b2de8d7430d53
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-10-27 16:54:27 -0300

    fix: abstraindo pdf

--- packages/apps/web/src/pages/PDF/index.tsx
+++ packages/apps/web/src/pages/PDF/index.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
-import PDFRead from '@psdhub/pdf'
+import WebViewer from '@pdftron/webviewer'
 
-const PDF: React.FC = () => {
-  return <PDFRead />
+import { Container } from './styles'
+
+const PdfProject: React.FC = () => {
+  const viewer = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    WebViewer(
+      {
+        path: '/lib',
+
+        initialDoc:
+          'https://stlivromusicaprod001.blob.core.windows.net/audios/CTPM_Mergulhando_em_poesia.pdf',
+        disableLogs: true,
+        isReadOnly: true,
+        licenseKey:
+          'Studos Software Ltda(studos.com.br):OEM:Studos::B+:AMS(20220809):C1DC662C07FAFAF38B313BC9B243182F4E6F3FD7763A639BB57BB43C404EB604F431F5C7',
+        streaming: true
+      },
+      viewer.current as HTMLElement
+    ).then(instance => {
+      instance.UI.disableElements([
+        'printButton',
+        'downloadButton',
+        'themeChangeButton',
+        'thumbDelete',
+        'pageManipulationOverlayButton',
+        'toolbarGroup-View',
+        'toggleNotesButton',
+        'thumbMultiDelete',
+        'thumbExtract',
+        'selectToolButton'
+      ])
+
+      instance.UI.setLanguage('pt_BR')
+    })
+  }, [])
+
+  return <Container className="webviewer" ref={viewer}></Container>
 }
 
-export default PDF
+export default PdfProject
