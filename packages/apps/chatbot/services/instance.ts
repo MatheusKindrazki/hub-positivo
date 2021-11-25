618f0fd29e5ca9206d8cb350124b37c1c35fb361
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-11-25 10:56:20 -0300

    feat: aplicando logica para aparecimento do bot

---
+++ packages/apps/chatbot/services/instance.ts
@@ -0,0 +1,19 @@
+const availableSchools = ['87909a2c-7ec1-4103-bf12-83467e69fd9a']
+
+const availableProfiles = ['professor', 'administrador', ' coordenador']
+
+interface ChatbotInstance {
+  school: string
+  profile: string
+}
+
+function initChatbot(data: ChatbotInstance): void {
+  const { profile, school } = data
+
+  if (!availableSchools.includes(school)) return
+
+  if (!availableProfiles.includes(profile)) return
+
+  require('../scripts/zoho')
+}
+export { initChatbot }
