0ca49074874f58760d1f5e777b1f2841cfda69ee
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-11-25 16:57:43 -0300

    feat: removendo chatbot no logout

--- packages/apps/chatbot/services/instance.ts
+++ packages/apps/chatbot/services/instance.ts
@@ -1,6 +1,6 @@
 const availableSchools = ['87909a2c-7ec1-4103-bf12-83467e69fd9a']
 
-const availableProfiles = ['PROFESSOR', 'ADMINISTRADOR', ' COORDENADOR']
+const availableProfiles = ['PROFESSOR', 'ADMINISTRADOR', 'COORDENADOR']
 
 interface ChatbotInstance {
   school: string
@@ -16,4 +16,18 @@ function initChatbot(data: ChatbotInstance): void {
 
   require('../scripts/zoho')
 }
-export { initChatbot }
+
+declare global {
+  interface Window {
+    $zoho: any
+  }
+}
+
+function removeChatbot(): void {
+  window.$zoho = undefined
+
+  const zoho = document.getElementById('zsiqscript')
+
+  zoho?.remove()
+}
+export { initChatbot, removeChatbot }
