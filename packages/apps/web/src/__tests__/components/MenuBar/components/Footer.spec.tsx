7b1c8cf7e54ead2dc775de0d9934a282e2f069b0
gabryelryba <gabryel.ryba@gmail.com>
2022-01-07 11:02:34 -0300

    test: alinhamento de testes do footer do menu lateral

--- packages/apps/web/src/__tests__/components/MenuBar/components/Footer.spec.tsx
+++ packages/apps/web/src/__tests__/components/MenuBar/components/Footer.spec.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { fireEvent } from '@testing-library/dom'
 
+import { store } from '~/store'
+
 import { render } from '@psdhub/test-utils'
 import { Drawer } from '@psdhub/common/components'
 
@@ -12,23 +14,40 @@ describe('MenuFooter should work properly', () => {
     jest.clearAllMocks()
   })
 
-  const setup = () => {
+  const setup = (guid: string) => {
     const handleSignOut = jest.fn()
     const openModalPass = jest.fn()
     const openModalVersionUpdate = jest.fn()
+    const redirectToMyClasses = jest.fn()
     const wrapper = render(
       <Drawer isOpen={true} onClose={jest.fn()}>
         <MenuFooter
+          redirectToMyClasses={redirectToMyClasses}
           handleSignOut={handleSignOut}
           openModalPass={openModalPass}
           openModalVersionUpdate={openModalVersionUpdate}
         />
-      </Drawer>
+      </Drawer>,
+      {
+        store: store,
+        reducers: ['profile'],
+        CUSTOM_STATE: {
+          profile: {
+            guid: guid
+          }
+        }
+      }
     )
-    return { handleSignOut, openModalPass, openModalVersionUpdate, ...wrapper }
+    return {
+      handleSignOut,
+      openModalPass,
+      openModalVersionUpdate,
+      redirectToMyClasses,
+      ...wrapper
+    }
   }
   it('Should render the correct elements on screen', () => {
-    const { queryByText } = setup()
+    const { queryByText } = setup('ALUNO')
     const signOut = queryByText(/sair/i)
     const modalPass = queryByText(/alterar minha senha/i)
 
@@ -37,9 +56,14 @@ describe('MenuFooter should work properly', () => {
   })
 
   it('Should should call the handlers functions when its buttons is clicked', () => {
-    const { getByText, openModalPass, handleSignOut } = setup()
+    const { getByText, openModalPass, handleSignOut, redirectToMyClasses } =
+      setup('PROFESSOR')
     const signOut = getByText(/sair/i)
     const modalPass = getByText(/alterar minha senha/i)
+    const myClasses = getByText(/Minhas turmas/)
+
+    fireEvent.click(myClasses)
+    expect(redirectToMyClasses).toHaveBeenCalled()
 
     fireEvent.click(signOut)
     expect(handleSignOut).toHaveBeenCalled()
