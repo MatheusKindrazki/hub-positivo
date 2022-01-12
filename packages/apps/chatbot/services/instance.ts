cbc07025caf88b94b1c7a6e0ae377f9214d181dd
Matheus Kindrazki <matheuskindrazki@gmail.com>
2022-01-12 15:15:35 -0300

    refactor: removendo validação de escola no chatbot

--- packages/apps/chatbot/services/instance.ts
+++ packages/apps/chatbot/services/instance.ts
@@ -1,24 +1,4 @@
-const availableSchools = [
-  '87909a2c-7ec1-4103-bf12-83467e69fd9a',
-  '57f5d301-b83b-4d49-8eed-17c55444fdd5',
-  '6aaede1a-d1e4-469d-8a64-d79cfd623e2e',
-  '6c8cc11f-1e60-4918-96f9-acf6afbabe32'
-]
-
-const availableProfiles = ['PROFESSOR', 'ADMINISTRADOR', 'COORDENADOR']
-
-interface ChatbotInstance {
-  school: string
-  profile: string
-}
-
-function initChatbot(data: ChatbotInstance): void {
-  const { profile, school } = data
-
-  if (!availableSchools.includes(school)) return
-
-  if (!availableProfiles.includes(profile)) return
-
+function initChatbot(): void {
   require('../scripts/zoho')
 }
 
