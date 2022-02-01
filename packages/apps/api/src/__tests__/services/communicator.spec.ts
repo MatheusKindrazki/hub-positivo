04b5302eef4d81ecba1fb85c63fcc49367acf1d2
gabryelryba <gabryel.ryba@gmail.com>
2022-02-01 17:59:59 -0300

    test: aumentando coverage do test de communicators

--- packages/apps/api/src/__tests__/services/communicator.spec.ts
+++ packages/apps/api/src/__tests__/services/communicator.spec.ts
@@ -3,6 +3,7 @@ import { sample } from 'lodash'
 import {
   getInstance,
   setAuthorization,
+  removeAuthorization,
   Variant,
   communicationURLs
 } from '../../services/communicators'
@@ -13,10 +14,28 @@ describe('API Communicators', () => {
     expect(communicator).toBeDefined()
   })
 
-  it('Should set Authorization header', () => {
-    const communicator = getInstance()
-    setAuthorization('test')
-    expect(communicator.headers.Authorization).toBe('Bearer test')
+  describe('Should set and remove Authorization properly', () => {
+    it('On default communicator when parameter is ommited', () => {
+      const communicator = getInstance()
+      setAuthorization('test')
+      expect(communicator.headers.Authorization).toBe('Bearer test')
+
+      removeAuthorization()
+      expect(communicator.headers.Authorization).toBe(undefined)
+    })
+
+    it('On all communicators when "all" parameter is received', () => {
+      const defaultCommunicator = getInstance()
+      const authCommunicator = getInstance('auth')
+
+      setAuthorization('test', 'all')
+      expect(defaultCommunicator.headers.Authorization).toBe('Bearer test')
+      expect(authCommunicator.headers.Authorization).toBe('Bearer test')
+
+      removeAuthorization('all')
+      expect(defaultCommunicator.headers.Authorization).toBe(undefined)
+      expect(authCommunicator.headers.Authorization).toBe(undefined)
+    })
   })
 
   it('Should set Authorization all APIs', () => {
@@ -25,6 +44,11 @@ describe('API Communicators', () => {
     const randomKey = sample(Object.keys(communicationURLs)) as Variant
 
     const communicator = getInstance(randomKey)
+    expect(communicator.headers.Authorization).toBe('Bearer test')
+  })
+  it('Should remove specifig Authorization header', () => {
+    const communicator = getInstance()
+    setAuthorization('test')
     expect(communicator.headers.Authorization).toBe('Bearer test')
   })
 })
