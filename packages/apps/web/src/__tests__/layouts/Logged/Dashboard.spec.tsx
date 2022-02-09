2bb4df0b0ceb2b4a4b8652117af6ebad5d2bf028
Hebert Silva <hebertsilva@positivosolucoes.com.br>
2022-02-09 17:57:29 -0300

    test: ajuste em teste do componente de Layout Logged

--- packages/apps/web/src/__tests__/layouts/Logged/Dashboard.spec.tsx
+++ packages/apps/web/src/__tests__/layouts/Logged/Dashboard.spec.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { StepsTour } from '~/store/modules/tour/types'
 import { store } from '~/store'
 
-import { render, CustomState, fireEvent, waitFor } from '@psdhub/test-utils'
+import { render, CustomState, fireEvent } from '@psdhub/test-utils'
 
 import * as mixpanel from '~/services/mixpanel/setProperties'
 import history from '~/services/history'
@@ -140,7 +140,7 @@ describe('Logged`s layout should render without crashing', () => {
     expect(secondStep).toBeNull()
   })
 
-  it('Steps should work properly on Dashboard when `open` and `viewed` are true', async () => {
+  it('Steps should work properly on Dashboard when `open` and `viewed` are true', () => {
     const { getByText, getAllByRole, storeUtils } = setup('children', {
       tour: {
         open: true,
@@ -171,7 +171,7 @@ describe('Logged`s layout should render without crashing', () => {
       { payload: { open: false }, type: '@tour/OPEN_TOUR' }
     ])
   })
-  it('Steps should work properly on Dashboard when `open` is true and `viewed` is false', async () => {
+  it('Steps should work properly on Dashboard when `open` is true and `viewed` is false', () => {
     const { storeUtils, getAllByRole } = setup('children', {
       tour: {
         open: true,
@@ -201,14 +201,14 @@ describe('Logged`s layout should render without crashing', () => {
     expect(queryByText(/Modal Alternative Access/i)).toBeInTheDocument()
   })
 
-  it('should return user at goBack button', async () => {
+  it('should return user at goBack button', () => {
     jest.useFakeTimers()
 
     const spyPush = jest.spyOn(history, 'push')
     const { getByText } = setup('children')
 
     expect(getByText('Header')).toBeInTheDocument()
-    await waitFor(() => fireEvent.click(getByText('Header')))
+    fireEvent.click(getByText('Header'))
 
     jest.runAllTimers()
 
